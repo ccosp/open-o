7c5701389b9360c0a01805eeb62010bd50e312d9
Colcamex Resources Inc <dwarren@colcamex.com>
2020-12-07 22:43:33 -0800

    forgot-to-add-rbt-group-schema

--- database/mysql/oscarinit.sql
+++ database/mysql/oscarinit.sql
@@ -12333,6 +12333,11 @@ CREATE TABLE IF NOT EXISTS SystemPreferences
   updateDate DATETIME    NULL
 );
 
-
+CREATE TABLE IF NOT EXISTS `rbt_groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tid` int(11) DEFAULT NULL,
+  `group_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
 
 
---
+++ database/mysql/updates/update-2020-12-08.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `rbt_groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tid` int(11) DEFAULT NULL,
+  `group_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
