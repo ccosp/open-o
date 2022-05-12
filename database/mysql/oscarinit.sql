966d0935296efed54a34d3b93592c5c7732b4d42
Fred Ngo <fredngo@gmail.com>
2022-05-12 19:46:36 +0000

    Add missing EFormDocs table from update-2019-10-09.sql

--- database/mysql/oscarinit.sql
+++ database/mysql/oscarinit.sql
@@ -12136,6 +12136,16 @@ CREATE TABLE IF NOT EXISTS specialty (
   specialtydesc varchar(100) default ''
 ) ;
 
+CREATE TABLE IF NOT EXISTS `EFormDocs` (
+  `id` int(10) NOT NULL auto_increment PRIMARY KEY,
+  `fdid` int(10) NOT NULL,
+  `document_no` int(10) NOT NULL,
+  `doctype` char(1) NOT NULL,
+  `deleted` char(1) DEFAULT NULL,
+  `attach_date` date,
+  `provider_no` varchar(6) NOT NULL
+) ;
+
 CREATE TABLE IF NOT EXISTS EFormReportTool (
   `id` int(11) NOT NULL auto_increment,
   `tableName` varchar(255) not null,
