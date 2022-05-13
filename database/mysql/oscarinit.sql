bd95eacf327ca61434d8e779e5494b8a4bf56f17
Fred Ngo <fredngo@gmail.com>
2022-05-13 18:45:49 +0000

    Add missing PreventionReport table and seed data for report_letters, from update-2019-03-08.sql.
    Created an oscardata_additional.sql file to place huge seed data or else oscardata.sql becomes unmanageable... it already kind of is unmanageable.

--- database/mysql/oscarinit.sql
+++ database/mysql/oscarinit.sql
@@ -7174,6 +7174,23 @@ CREATE TABLE IF NOT EXISTS prescription (
 ) ;
 
 --
+-- Table structure for table `PreventionReport`
+--
+
+CREATE TABLE IF NOT EXISTS `PreventionReport` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `providerNo` varchar(6) DEFAULT NULL,
+  `reportName` varchar(255) DEFAULT NULL,
+  `json` text,
+  `updateDate` datetime DEFAULT NULL,
+  `createDate` datetime DEFAULT NULL,
+  `active` tinyint(1) DEFAULT NULL,
+  `archived` tinyint(1) DEFAULT NULL,
+  `uuid` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ;
+
+--
 -- Table structure for table `professionalSpecialists`
 --
 
