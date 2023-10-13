9746beddec860b888b03d26ab2748602d33e3195
Deval <deval.italiya@magentahealth.ca>
2023-10-13 13:28:40 +0000

    Made some minor changes and using an enum instead of a string for 'docType'

--- src/main/java/org/oscarehr/documentManager/DocumentAttach.java
+++ src/main/java/org/oscarehr/documentManager/DocumentAttach.java
@@ -4,6 +4,7 @@ import org.oscarehr.common.dao.ConsultDocsDao;
 import org.oscarehr.common.dao.EFormDocsDao;
 import org.oscarehr.common.model.ConsultDocs;
 import org.oscarehr.common.model.EFormDocs;
+import org.oscarehr.common.model.enumerator.DocumentType;
 import org.oscarehr.util.SpringUtils;
 
 import java.util.ArrayList;
@@ -11,35 +12,35 @@ import java.util.Arrays;
 import java.util.List;
 
 public class DocumentAttach {
-	ConsultDocsDao consultDocsDao = SpringUtils.getBean(ConsultDocsDao.class);
-	EFormDocsDao eFormDocsDao = SpringUtils.getBean(EFormDocsDao.class);
+	private final ConsultDocsDao consultDocsDao = SpringUtils.getBean(ConsultDocsDao.class);
+	private final EFormDocsDao eFormDocsDao = SpringUtils.getBean(EFormDocsDao.class);
 
 	public DocumentAttach() {
 	}
 
-	public void attachToConsult(String[] attachments, String docType, String providerNo, Integer requestId) {
+	public void attachToConsult(String[] attachments, DocumentType documentType, String providerNo, Integer requestId) {
 		List<String> currentList = new ArrayList<>(Arrays.asList(attachments));
-		List<ConsultDocs> consultDocsList = consultDocsDao.findByRequestIdDocType(requestId, docType);
+		List<ConsultDocs> consultDocsList = consultDocsDao.findByRequestIdDocType(requestId, documentType.getType());
 		List<String> oldList = new ArrayList<>();
 		for (ConsultDocs consultDoc : consultDocsList) {
 			oldList.add(Integer.toString(consultDoc.getDocumentNo()));
 		}
-		detachFromConsult(currentList, oldList, docType, requestId);
-		attachToConsult(currentList, oldList, docType, providerNo, requestId);
+		detachFromConsult(currentList, oldList, documentType, requestId);
+		attachToConsult(currentList, oldList, documentType, providerNo, requestId);
 	}
 
-	private void attachToConsult(List<String> currentList, List<String> oldList, String docType, String providerNo, Integer requestId) {
+	private void attachToConsult(List<String> currentList, List<String> oldList, DocumentType documentType, String providerNo, Integer requestId) {
 		for (String docId : currentList) {
 			if (oldList.contains(docId)) { continue; }
-			ConsultDocs consultDoc = new ConsultDocs(requestId, Integer.parseInt(docId), docType, providerNo);
+			ConsultDocs consultDoc = new ConsultDocs(requestId, Integer.parseInt(docId), documentType.getType(), providerNo);
 			consultDocsDao.persist(consultDoc);
 		}
 	}
 
-	private void detachFromConsult(List<String> currentList, List<String> oldList, String docType, Integer requestId) {
+	private void detachFromConsult(List<String> currentList, List<String> oldList, DocumentType documentType, Integer requestId) {
 		for (String docId : oldList) {
 			if (currentList.contains(docId)) { continue; }
-			List<ConsultDocs> detachList = consultDocsDao.findByRequestIdDocNoDocType(requestId, Integer.valueOf(docId), docType);
+			List<ConsultDocs> detachList = consultDocsDao.findByRequestIdDocNoDocType(requestId, Integer.valueOf(docId), documentType.getType());
 			for (ConsultDocs consultDoc : detachList) {
 				consultDoc.setDeleted("Y");
 				consultDocsDao.merge(consultDoc);
@@ -47,29 +48,29 @@ public class DocumentAttach {
 		}
 	}
 
-	public void attachToEForm(String[] attachments, String docType, String providerNo, Integer fdid) {
+	public void attachToEForm(String[] attachments, DocumentType documentType, String providerNo, Integer fdid) {
 		List<String> currentList = new ArrayList<>(Arrays.asList(attachments));
-		List<EFormDocs> eFormDocsList = eFormDocsDao.findByFdidIdDocType(fdid, docType);
+		List<EFormDocs> eFormDocsList = eFormDocsDao.findByFdidIdDocType(fdid, documentType.getType());
 		List<String> oldList = new ArrayList<>();
 		for (EFormDocs eFormDoc : eFormDocsList) {
 			oldList.add(Integer.toString(eFormDoc.getDocumentNo()));
 		}
-		detachFromEForm(currentList, oldList, docType, fdid);
-		attachToEForm(currentList, oldList, docType, providerNo, fdid);
+		detachFromEForm(currentList, oldList, documentType, fdid);
+		attachToEForm(currentList, oldList, documentType, providerNo, fdid);
 	}
 
-	private void attachToEForm(List<String> currentList, List<String> oldList, String docType, String providerNo, Integer fdid) {
+	private void attachToEForm(List<String> currentList, List<String> oldList, DocumentType documentType, String providerNo, Integer fdid) {
 		for (String docId : currentList) {
 			if (oldList.contains(docId)) { continue; }
-			EFormDocs eFormDocs = new EFormDocs(fdid, Integer.parseInt(docId), docType, providerNo);
+			EFormDocs eFormDocs = new EFormDocs(fdid, Integer.parseInt(docId), documentType.getType(), providerNo);
 			eFormDocsDao.persist(eFormDocs);
 		}
 	}
 
-	private void detachFromEForm(List<String> currentList, List<String> oldList, String docType, Integer fdid) {
+	private void detachFromEForm(List<String> currentList, List<String> oldList, DocumentType documentType, Integer fdid) {
 		for (String docId : oldList) {
 			if (currentList.contains(docId)) { continue; }
-			List<EFormDocs> detachList = eFormDocsDao.findByFdidIdDocNoDocType(fdid, Integer.valueOf(docId), docType);
+			List<EFormDocs> detachList = eFormDocsDao.findByFdidIdDocNoDocType(fdid, Integer.valueOf(docId), documentType.getType());
 			for (EFormDocs eFormDoc : detachList) {
 				eFormDoc.setDeleted("Y");
 				eFormDocsDao.merge(eFormDoc);
--- src/main/java/org/oscarehr/documentManager/DocumentAttachmentManager.java
+++ src/main/java/org/oscarehr/documentManager/DocumentAttachmentManager.java
@@ -4,9 +4,9 @@ import org.oscarehr.common.dao.ConsultDocsDao;
 import org.oscarehr.common.dao.EFormDocsDao;
 import org.oscarehr.common.model.ConsultDocs;
 import org.oscarehr.common.model.EFormDocs;
-import org.oscarehr.managers.*;
+import org.oscarehr.managers.SecurityInfoManager;
+import org.oscarehr.common.model.enumerator.DocumentType;
 import org.oscarehr.util.LoggedInInfo;
-import org.oscarehr.util.SpringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,50 +16,59 @@ import java.util.List;
 @Service
 public class DocumentAttachmentManager {
 	@Autowired
-	SecurityInfoManager securityInfoManager;
-	ConsultDocsDao consultDocsDao = SpringUtils.getBean(ConsultDocs.class);
-	EFormDocsDao eFormDocsDao = SpringUtils.getBean(EFormDocsDao.class);
-	DocumentAttach documentAttach = new DocumentAttach();
+	private SecurityInfoManager securityInfoManager;
+	@Autowired
+	private ConsultDocsDao consultDocsDao;
+	@Autowired
+	private EFormDocsDao eFormDocsDao;
+	private final DocumentAttach documentAttach = new DocumentAttach();
 
-	public List<Integer> getConsultAttachments(LoggedInInfo loggedInInfo, Integer requestId, String docType, Integer demographicNo) {
+	/*
+	 * Note to developer, after completly implementing DocumentAttachmentManager and DocumentAttach
+	 * Move out requestId/fdid, providerNo, and demographicNo and define it using
+	 * DocumentAttachmentManager/DocumentAttach constructor and getter/setter
+	 * By doing that it will decrease number parameters(MAX 3) in methods - right way of coding
+	 */
+
+	public List<Integer> getConsultAttachments(LoggedInInfo loggedInInfo, Integer requestId, DocumentType documentType, Integer demographicNo) {
 		if (!securityInfoManager.hasPrivilege(loggedInInfo, "_con", SecurityInfoManager.READ, demographicNo)) {
 			throw new RuntimeException("missing required security object (_con)");
 		}
 
 		List<Integer> consultAttachments = new ArrayList<>();
-		List<ConsultDocs> consultDocs = consultDocsDao.findByRequestIdDocType(requestId, docType);
+		List<ConsultDocs> consultDocs = consultDocsDao.findByRequestIdDocType(requestId, documentType.getType());
 		for (ConsultDocs consultDocs1 : consultDocs) {
 			consultAttachments.add(consultDocs1.getDocumentNo());
 		}
 		return consultAttachments;
 	}
 
-	public List<Integer> getEFormAttachments(LoggedInInfo loggedInInfo, Integer fdid, String docType, Integer demographicNo) {
+	public List<Integer> getEFormAttachments(LoggedInInfo loggedInInfo, Integer fdid, DocumentType documentType, Integer demographicNo) {
 		if (!securityInfoManager.hasPrivilege(loggedInInfo, "_eform", SecurityInfoManager.READ, demographicNo)) {
 			throw new RuntimeException("missing required security object (_eform)");
 		}
 
 		List<Integer> eFormAttachments = new ArrayList<>();
-		List<EFormDocs> eFormDocs = eFormDocsDao.findByFdidIdDocType(fdid, docType);
+		List<EFormDocs> eFormDocs = eFormDocsDao.findByFdidIdDocType(fdid, documentType.getType());
 		for (EFormDocs eFormDocs1 : eFormDocs) {
 			eFormAttachments.add(eFormDocs1.getDocumentNo());
 		}
 		return eFormAttachments;
 	}
 
-	public void attachToConsult(LoggedInInfo loggedInInfo, String docType, String[] attachments, String providerNo, Integer requestId, Integer demographicNo) {
+	public void attachToConsult(LoggedInInfo loggedInInfo, DocumentType documentType, String[] attachments, String providerNo, Integer requestId, Integer demographicNo) {
 		if (!securityInfoManager.hasPrivilege(loggedInInfo, "_con", SecurityInfoManager.WRITE, demographicNo)) {
 			throw new RuntimeException("missing required security object (_con)");
 		}
 
-		documentAttach.attachToConsult(attachments, docType, providerNo, requestId);
+		documentAttach.attachToConsult(attachments, documentType, providerNo, requestId);
 	}
 
-	public void attachToEForm(LoggedInInfo loggedInInfo, String docType, String[] attachments, String providerNo, Integer fdid, Integer demographicNo) {
+	public void attachToEForm(LoggedInInfo loggedInInfo, DocumentType documentType, String[] attachments, String providerNo, Integer fdid, Integer demographicNo) {
 		if (!securityInfoManager.hasPrivilege(loggedInInfo, "_eform", SecurityInfoManager.WRITE, demographicNo)) {
 			throw new RuntimeException("missing required security object (_eform)");
 		}
 
-		documentAttach.attachToEForm(attachments, docType, providerNo, fdid);
+		documentAttach.attachToEForm(attachments, documentType, providerNo, fdid);
 	}
 }
