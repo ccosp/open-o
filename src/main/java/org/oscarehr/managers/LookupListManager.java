f23657763f4ac9a6fe921e37082c3c3c803e4ddb
Colcamex Resources Inc <dwarren@colcamex.com>
2024-01-30 11:05:07 -0800

    reduce log chatter

--- src/main/java/org/oscarehr/managers/LookupListManager.java
+++ src/main/java/org/oscarehr/managers/LookupListManager.java
@@ -90,20 +90,11 @@ public class LookupListManager {
 	 * Retrieve all the active select list option items by the lookUpList.id
 	 */
 	public List<LookupListItem> findLookupListItemsByLookupListId(LoggedInInfo loggedInInfo, int lookupListId ) {
-
-		List<LookupListItem> lookupListItems = lookupListItemDao.findActiveByLookupListId( lookupListId );
-
-		if ( lookupListItems != null ) {
-			LogAction.addLogSynchronous(loggedInInfo, "LookupListManager.findLookupListItemsByLookupListId", lookupListItems.toString() );
-		}
-
-		return lookupListItems;
+		return lookupListItemDao.findActiveByLookupListId( lookupListId );
 	}
 	
 	public LookupListItem findLookupListItemByLookupListIdAndValue(LoggedInInfo loggedInInfo, int lookupListId, String value)  {
-		
 		return lookupListItemDao.findByLookupListIdAndValue(lookupListId, value);
-
 	}
 	
 
@@ -117,7 +108,6 @@ public class LookupListManager {
 
 		if (lookupList != null) {
 			lookupListItems = findLookupListItemsByLookupListId(loggedInInfo, lookupList.getId() );
-			LogAction.addLogSynchronous(loggedInInfo, "LookupListManager.findLookupListItemsByLookupListName", lookupList.toString() );
 		}
 
 		return lookupListItems;
@@ -132,9 +122,6 @@ public class LookupListManager {
 		LookupListItem lookupListItem = null;
 		if( lookupListItemId > 0 ) {		
 			lookupListItem = lookupListItemDao.find( lookupListItemId );
-		}
-		if( lookupListItem != null ) {
-			LogAction.addLogSynchronous(loggedInInfo, "LookupListManager.findLookupListItemById", lookupListItem.toString());
 		}
 		return lookupListItem;
 	}
@@ -179,7 +166,6 @@ public class LookupListManager {
 	/**
 	 * Change the display order sequence of this lookupListItem
 	 * @param lookupListItemId
-	 * @param displayOrder
 	 */
 	public boolean updateLookupListItemDisplayOrder(LoggedInInfo loggedInInfo, int lookupListItemId, int lookupListItemDisplayOrder ) { 
 
