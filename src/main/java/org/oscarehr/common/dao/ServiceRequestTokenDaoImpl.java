30fc1a170812a8b1b53c68804aab2e98c1b84ae1
Ayush <Akaneria@conestogac.on.ca>
2024-03-22 21:28:50 +0000

    Added interface and some code changes

---
+++ src/main/java/org/oscarehr/common/dao/ServiceRequestTokenDaoImpl.java
@@ -0,0 +1,42 @@
+package org.oscarehr.common.dao;
+ 
+import javax.persistence.Query;
+import org.oscarehr.common.model.ServiceRequestToken;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+ 
+@Repository
+public class ServiceRequestTokenDaoImpl extends AbstractDao<ServiceRequestToken> implements ServiceRequestTokenDao{
+ 
+    public ServiceRequestTokenDaoImpl() {
+        super(ServiceRequestToken.class);
+    }
+
+    @Override
+public void persist(ServiceRequestToken token) {
+    this.entityManager.persist(token);
+}
+ 
+@Override
+public void remove(ServiceRequestToken token) {
+    this.entityManager.remove(token);
+}
+ 
+@Override
+public ServiceRequestToken merge(ServiceRequestToken token) {
+    return this.entityManager.merge(token);
+}
+ 
+    @Override
+    public List<ServiceRequestToken> findAll() {
+        Query query = this.entityManager.createQuery("SELECT x FROM ServiceRequestToken x", ServiceRequestToken.class);
+        return query.getResultList();
+    }
+ 
+    @Override
+    public ServiceRequestToken findByTokenId(String token) {
+        Query query = this.entityManager.createQuery("SELECT x FROM ServiceRequestToken x WHERE x.tokenId = :token", ServiceRequestToken.class);
+        query.setParameter("token", token);
+        return this.getSingleResultOrNull(query);
+    }
+}
