d0996db22a02d9821dee0c92b13282b5ebe07069
Ayush <Akaneria@conestogac.on.ca>
2024-04-03 15:49:29 +0000

    Added interface for ResourceStorageDao

---
+++ src/main/java/org/oscarehr/common/dao/ResourceStorageDaoImpl.java
@@ -0,0 +1,42 @@
+package org.oscarehr.common.dao;
+
+
+import java.util.List;
+
+import javax.persistence.Query;
+
+import org.oscarehr.common.model.ResourceStorage;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ResourceStorageDaoImpl extends AbstractDaoImpl<ResourceStorage> implements ResourceStorageDao{
+
+	public ResourceStorageDaoImpl() {
+		super(ResourceStorage.class);
+	}
+
+	public ResourceStorage findActive(String resourceType){
+		Query query = entityManager.createQuery("FROM " + modelClass.getSimpleName() + " r WHERE r.resourceType = :resourceType AND r.active = true");
+		query.setParameter("resourceType", resourceType);
+		return getSingleResultOrNull(query);
+	}
+	
+	public List<ResourceStorage> findActiveAll(String resourceType){
+		Query query = entityManager.createQuery("FROM " + modelClass.getSimpleName() + " r WHERE r.resourceType = :resourceType AND r.active = true");
+		query.setParameter("resourceType", resourceType);
+		return query.getResultList();
+	}
+
+	public List<ResourceStorage> findAll(String resourceType){
+		Query query = entityManager.createQuery("FROM " + modelClass.getSimpleName() + " r WHERE r.resourceType = :resourceType ");
+		query.setParameter("resourceType", resourceType);
+		return query.getResultList();
+	}
+	
+	public List<ResourceStorage> findByUUID(String uuid){
+		Query query = entityManager.createQuery("FROM " + modelClass.getSimpleName() + " r WHERE r.uuid = :uuid ");
+		query.setParameter("uuid", uuid);
+		return query.getResultList();
+	}
+	
+}
