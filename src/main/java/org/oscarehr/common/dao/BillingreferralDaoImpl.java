5cbe754df4a2c705e41b31625b55097b0a57ecdb
Ayush <Akaneria@conestogac.on.ca>
2024-04-05 15:54:39 +0000

    Refactored BillingreferralDao and BillingreferralDaoImpl classes.

--- src/main/java/org/oscarehr/common/dao/BillingreferralDaoImpl.java
+++ src/main/java/org/oscarehr/common/dao/BillingreferralDaoImpl.java
@@ -43,18 +43,143 @@ public class BillingreferralDaoImpl extends AbstractDaoImpl<Billingreferral> imp
 
 	@Override
 	public Billingreferral getByReferralNo(String referral_no) {
-		// existing code
+		String sql = "select br From Billingreferral br WHERE br.referralNo=?";
+		Query query = entityManager.createQuery(sql);
+		query.setParameter(1, referral_no);
+
+		@SuppressWarnings("unchecked")
+		List<Billingreferral> brs = query.getResultList();
+		if(!brs.isEmpty())
+			return brs.get(0);
+		return null;
 	}
 
 	@Override
 	public Billingreferral getById(int id) {
-		// existing code
+		return find(id);
 	}
 
 	@Override
 	public List<Billingreferral> getBillingreferrals() {
-		// existing code
+		String sql = "SELECT br From Billingreferral br ORDER BY  br.lastName, br.firstName";
+		Query query = entityManager.createQuery(sql);
+
+		@SuppressWarnings("unchecked")
+		List<Billingreferral> brs = query.getResultList();
+
+		return brs;
 	}
 
-	// similar changes for other methods
+	@Override
+	public List<Billingreferral> getBillingreferral(String referral_no) {
+	   String sql = "SELECT br From Billingreferral br WHERE br.referralNo=?";
+	   Query query = entityManager.createQuery(sql);
+	   query.setParameter(1, referral_no);
+
+	   @SuppressWarnings("unchecked")
+	   List<Billingreferral> cList = query.getResultList();
+
+	   if (cList != null && cList.size() > 0) {
+		   return cList;
+	   } else {
+		   return null;
+	   }
+   }
+
+   @Override  
+   public List<Billingreferral> getBillingreferral(String last_name, String first_name) {
+	   String sql = "SELECT br From Billingreferral br WHERE br.lastName like ? and br.firstName like ? order by br.lastName";
+	   Query query = entityManager.createQuery(sql);
+	   query.setParameter(1, "%"+last_name+"%");
+	   query.setParameter(2, "%"+first_name+"%");
+
+	   @SuppressWarnings("unchecked")
+	   List<Billingreferral> cList = query.getResultList();
+
+	   if (cList != null && cList.size() > 0) {
+		   return cList;
+	   } else {
+		   return null;
+	   }
+   }
+
+   @Override  
+   public List<Billingreferral> getBillingreferralByLastName(String last_name) {
+	   String sql = "SELECT br From Billingreferral br WHERE br.lastName like ? order by br.lastName";
+	   Query query = entityManager.createQuery(sql);
+	   query.setParameter(1, "%"+last_name+"%");
+
+	   @SuppressWarnings("unchecked")
+	   List<Billingreferral> cList = query.getResultList();
+
+	   if (cList != null && cList.size() > 0) {
+		   return cList;
+	   } else {
+		   return null;
+	   }
+   }
+
+
+   @Override  
+   public List<Billingreferral> getBillingreferralBySpecialty(String specialty) {
+	   String sql = "SELECT br From Billingreferral br WHERE br.specialty like ? order by br.lastName";
+	   Query query = entityManager.createQuery(sql);
+	   query.setParameter(1, "%"+specialty+"%");
+
+	   @SuppressWarnings("unchecked")
+	   List<Billingreferral> cList = query.getResultList();
+
+	   if (cList != null && cList.size() > 0) {
+		   return cList;
+	   } else {
+		   return null;
+	   }
+   }
+
+   /*
+	* Don't blame me for this one, converted from SQL.
+	*/
+	@Override
+	public List<Billingreferral> searchReferralCode(String codeName, String codeName1, String codeName2, String desc, String fDesc, String desc1, String fDesc1, String desc2, String fDesc2) {
+	   String sql = "SELECT b FROM Billingreferral b WHERE b.referralNo LIKE ? or b.referralNo LIKE ? or b.referralNo LIKE ?"
+			   + " or (b.lastName LIKE ? and b.firstName LIKE ?)"
+			   + " or (b.lastName LIKE ? and b.firstName LIKE ?)"
+			   + " or (b.lastName LIKE ? and b.firstName LIKE ?)";
+
+	   Query query = entityManager.createQuery(sql);
+	   query.setParameter(1, codeName);
+	   query.setParameter(2, codeName1);
+	   query.setParameter(3, codeName2);
+	   query.setParameter(4, desc);
+	   query.setParameter(5, fDesc);
+	   query.setParameter(6, desc1);
+	   query.setParameter(7, fDesc1);
+	   query.setParameter(8, desc2);
+	   query.setParameter(9, fDesc2);
+
+
+	   @SuppressWarnings("unchecked")
+	   List<Billingreferral> cList = query.getResultList();
+
+	   return cList;
+   }
+
+   @Override  
+   public void updateBillingreferral(Billingreferral obj) {
+	   if(obj.getBillingreferralNo() == null || obj.getBillingreferralNo().intValue() == 0) {
+		   persist(obj);
+	   } else {
+		   merge(obj);
+	   }
+   }
+
+   @Override	
+   public String getReferralDocName(String referral_no) {
+		Billingreferral br = this.getByReferralNo(referral_no);
+
+		if(br != null)
+			return br.getLastName() + ", " + br.getFirstName();
+
+		return "";
+	}
 }
