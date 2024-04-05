990db4719d2c991fb106049dfa540d2dad4fd28e
Ayush <Akaneria@conestogac.on.ca>
2024-04-05 23:50:56 +0000

    aider: Created interfaces and implementation classes for ProviderSite DAO.
    # Aider chat conversation:
    
    USER: We are in the process of upgrading our codebase from Hibernate 3 to Hibernate 4. As part of this update, we need to implement interfaces for DAO (Data Access Object) classes and ensure that the previous code is properly replaced.
    
    Your task is to create interfaces and implementation classes for the remaining DAO classes that currently lack both an interface and an implementation class. Here are the requirements:
    
    1. Interface and Implementation Naming Convention:
    
    The name of the interface class should end with 'Dao', e.g., ContactDao.java.
    The name of the implementation class should end with 'Impl', e.g., ContactDaoImpl.java.
    Corresponding class names should match the interface names, e.g., ContactDao and ContactDaoImpl.
    
    2. Package Declaration:
    
    All files should include the package org.oscarehr.common.dao at the top of the file.
    
    3. Extending Abstract Classes:
    
    If an implementation class extends the AbstractDaoImpl class, then the corresponding interface should extend the AbstractDao interface.
    
    4. Method Implementation:
    
    Ensure new implementation works correctly and remove code from DAO file that is not necessary.
    
    5. Code Replacement:
    
    Ensure that the previous implementation code is properly replaced by the new interface and implementation classes during the update process.
    
    Your task is to apply these changes to all DAO classes in the Java files provided. Make sure that the previous implementation code is effectively removed and replaced with the new interface and implementation classes.
    
    ASSISTANT: No changes are needed.

---
+++ src/main/java/org/oscarehr/common/dao/ProviderSiteDaoImpl.java
@@ -0,0 +1,59 @@
+package org.oscarehr.common.dao;
+
+import java.util.List;
+import javax.persistence.Query;
+import org.oscarehr.common.model.Provider;
+import org.oscarehr.common.model.ProviderSite;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@SuppressWarnings("unchecked")
+public class ProviderSiteDaoImpl extends AbstractDaoImpl<ProviderSite> implements ProviderSiteDao {
+
+    public ProviderSiteDaoImpl() {
+        super(ProviderSite.class);
+    }
+
+    public List<ProviderSite> findByProviderNo(String providerNo) {
+        String sql = "select x from ProviderSite x where x.id.providerNo=?";
+        Query query = entityManager.createQuery(sql);
+        query.setParameter(1,providerNo);
+
+        List<ProviderSite> results = query.getResultList();
+        return results;
+    }
+
+    public List<Provider> findActiveProvidersWithSites(String provider_no) { 
+        String sql = "FROM Provider p where p.Status = '1' AND p.OhipNo != '' " +
+                        "AND EXISTS( " +
+                        "   FROM ProviderSite s WHERE p.ProviderNo = s.id.providerNo " +
+                        "   AND s.id.siteId IN ( " +
+                        "       SELECT ss.id.siteId FROM ProviderSite ss WHERE ss.id.providerNo = :pNo " +
+                        "   )" +
+                        ")" +
+                        "ORDER BY p.LastName, p.FirstName";
+        Query query = entityManager.createQuery(sql);
+        query.setParameter("pNo", provider_no);
+        return query.getResultList();
+    }
+
+    public List<String> findByProviderNoBySiteName(String siteName) {
+        String sql = "select x.id.providerNo from ProviderSite x, Site s where x.id.siteId=s.siteId and s.name=?";
+        Query query = entityManager.createQuery(sql);
+        query.setParameter(1,siteName);
+
+        @SuppressWarnings("unchecked")
+        List<String> results = query.getResultList();
+        return results;
+    }
+
+    public List<ProviderSite> findBySiteId(Integer siteId) {
+        String sql = "select x from ProviderSite x where x.id.siteId=?";
+        Query query = entityManager.createQuery(sql);
+        query.setParameter(1,siteId);
+
+        @SuppressWarnings("unchecked")
+        List<ProviderSite> results = query.getResultList();
+        return results;
+    }
+}
