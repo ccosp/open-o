855f40072bb102749c424db9569a50a0716d9d79
Ayush <Akaneria@conestogac.on.ca>
2024-04-05 21:45:34 +0000

    Added new methods to RelationshipsDao interface.

--- src/main/java/org/oscarehr/common/dao/RelationshipsDao.java
+++ src/main/java/org/oscarehr/common/dao/RelationshipsDao.java
@@ -105,3 +105,15 @@ public class RelationshipsDao extends AbstractDaoImpl<Relationships> {
 		return query.getResultList();
 	}
 }
+package org.oscarehr.common.dao;
+
+import java.util.List;
+import org.oscarehr.common.model.Relationships;
+
+public interface RelationshipsDao extends AbstractDao<Relationships> {
+    List<Relationships> findAll();
+    Relationships findActive(Integer id);
+    List<Relationships> findByDemographicNumber(Integer demographicNumber);
+    List<Relationships> findActiveSubDecisionMaker(Integer demographicNumber);
+    List<Relationships> findActiveByDemographicNumberAndFacility(Integer demographicNumber, Integer facilityId);
+}
