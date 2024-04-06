6ebc20380892b5cdc212920c817b582e09c4aed8
Ayush <Akaneria@conestogac.on.ca>
2024-04-06 03:15:46 +0000

    aider: Implemented SecurityDao interface and SecurityDaoImpl class following naming conventions and method implementations.
    # Aider chat conversation:
    
    USER: We are in the process of upgrading our codebase from Hibernate 3 to Hibernate 4. As part of this update, we need to implement interfaces for DAO (Data Access Object) classes and ensure that the previous code is properly replaced.
    
    Your task is to create interfaces and implementation classes for the remaining DAO classes that currently lack both an interface and an implementation class. Here are the requirements:
    
    1. Interface and Implementation Naming Convention:
    
    The name of the interface class should end with 'Dao', e.g., ContactDao.java.
    The name of the implementation class should end with 'Impl', e.g., ContactDaoImpl.java.
    Corresponding class names should match the interface names, e.g., ContactDao and ContactDaoImpl.
    
    2. Package Declaration:
    
    All files should include the package org.oscarehr.common.dao at the top of the file.
    
    3. Extending Abstract Classes:
    
    If an implementation class extends the AbstractDaoImpl class, then the corresponding interface should extend the AbstractDao interface.
    
    4. Method Implementation:
    
    Ensure new implementation works correctly and remove code from DAO file that is not necessary.
    
    5. Code Replacement:
    
    Ensure that the previous implementation code is properly replaced by the new interface and implementation classes during the update process.
    
    6. Import statement:
    
    Import statement should be unchanged in existing *Dao.java files
    
    Your task is to apply these changes to all DAO classes in the Java files provided. Make sure that the previous implementation code is effectively removed and replaced with the new interface and implementation classes.
    
    ASSISTANT: No changes are needed.

---
+++ src/main/java/org/oscarehr/common/dao/SecurityDaoImpl.java
@@ -0,0 +1,88 @@
+package org.oscarehr.common.dao;
+
+import java.util.List;
+import javax.persistence.Query;
+import org.oscarehr.common.model.Security;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SecurityDaoImpl extends AbstractDaoImpl<Security> implements SecurityDao {
+
+    public SecurityDaoImpl() {
+        super(Security.class);
+    }
+
+    public List<Security> findAllOrderBy(String columnName) {
+        Query query = entityManager.createQuery("SELECT s FROM Security s ORDER BY " + columnName);
+        @SuppressWarnings("unchecked")
+        List<Security> secList = query.getResultList();
+        return secList;
+    }
+
+    public List<Security> findByProviderNo(String providerNo) {
+        Query query = entityManager.createQuery("select x from Security x where x.providerNo=?");
+        query.setParameter(1, providerNo);
+        @SuppressWarnings("unchecked")
+        List<Security> secList = query.getResultList();
+        return secList;
+    }
+
+    public List<Security> findByLikeProviderNo(String providerNo) {
+        Query query = entityManager.createQuery("select x from Security x where x.providerNo like ?");
+        query.setParameter(1, providerNo);
+        @SuppressWarnings("unchecked")
+        List<Security> secList = query.getResultList();
+        return secList;
+    }
+
+    public List<Security> findByUserName(String userName) {
+        Query query = entityManager.createQuery("select x from Security x where x.userName=?");
+        query.setParameter(1, userName);
+        @SuppressWarnings("unchecked")
+        List<Security> secList = query.getResultList();
+        return secList;
+    }
+
+    public List<Security> findByOneIdKey(String ssoKey) {
+        Query query = entityManager.createQuery("SELECT x FROM Security x WHERE x.oneIdKey = ?");
+        query.setParameter(1, ssoKey);
+        @SuppressWarnings("unchecked")
+        List<Security> securityList = query.getResultList();
+        return securityList;
+    }
+
+    public void updateOneIdKey(Security securityRecord) {
+        merge(securityRecord);
+    }
+
+    public List<Security> findByLikeUserName(String userName) {
+        Query query = entityManager.createQuery("select x from Security x where x.userName like ?");
+        query.setParameter(1, userName);
+        @SuppressWarnings("unchecked")
+        List<Security> secList = query.getResultList();
+        return secList;
+    }
+
+    public Security getByProviderNo(String providerNo) {
+        List<Security> secList = this.findByProviderNo(providerNo);
+        if(secList.isEmpty())
+            return null;
+        return secList.get(0);
+    }
+
+    public List<Object[]> findProviders() {
+        String sql = "FROM Security s, Provider p WHERE p.providerNo = s.providerNo ORDER BY p.lastName";
+        Query query = entityManager.createQuery(sql);
+        return query.getResultList();
+    }
+
+    public List<Security> findByProviderSite(String providerNo) {
+        String queryStr = "select * from security s inner join providersite p on s.provider_no = p.provider_no " +
+                "where p.site_id in(select site_id from providersite where provider_no=?)";
+        Query query = entityManager.createNativeQuery(queryStr, modelClass);
+        query.setParameter(1, providerNo);
+        @SuppressWarnings("unchecked")
+        List<Security> secList = query.getResultList();
+        return secList;
+    }
+}
