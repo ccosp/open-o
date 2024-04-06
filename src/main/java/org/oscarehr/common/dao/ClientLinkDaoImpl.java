12eddecf75d5d313b79eed4ef5d7cc7af95341e0
Ayush <Akaneria@conestogac.on.ca>
2024-04-06 01:03:43 +0000

    aider: Created ClientLinkDao interface and ClientLinkDaoImpl implementation class, following naming conventions and extending AbstractDao classes.
    # Aider chat conversation:
    
    USER: We are in the process of upgrading our codebase from Hibernate 3 to Hibernate 4. As part of this update, we need to implement interfaces for DAO (Data Access Object) classes and ensure that the previous code is properly replaced.
    
    Your task is to create interfaces and implementation classes for the remaining DAO classes that currently lack both an interface and an implementation class. Here are the requirements:
    
    1. Interface and Implementation Naming Convention:
    
    The name of the interface class should end with 'Dao', e.g., ContactDao.java.
    The name of the implementation class should end with 'Impl', e.g., ContactDaoImpl.java.
    Corresponding class names should match the interface names, e.g., ContactDao and ContactDaoImpl.
    
    2. Package Declaration:
    
    All files should include the package org.oscarehr.common.dao at the top of the file.
    
    3. Extending Abstract Classes:
    
    If an implementation class extends the AbstractDaoImpl class, then the corresponding interface should extend the AbstractDao interface.
    
    4. Method Implementation:
    
    Ensure new implementation works correctly and remove code from DAO file that is not necessary.
    
    5. Code Replacement:
    
    Ensure that the previous implementation code is properly replaced by the new interface and implementation classes during the update process.
    
    6. Import statement:
    
    Import statement should be unchanged in existing *Dao.java files
    
    Your task is to apply these changes to all DAO classes in the Java files provided. Make sure that the previous implementation code is effectively removed and replaced with the new interface and implementation classes.
    
    ASSISTANT: No changes are needed.

--- src/main/java/org/oscarehr/common/dao/ClientLinkDaoImpl.java
+++ src/main/java/org/oscarehr/common/dao/ClientLinkDaoImpl.java
@@ -24,8 +24,38 @@
 package org.oscarehr.common.dao;
 
 import java.util.List;
+import javax.persistence.Query;
 import org.oscarehr.common.model.ClientLink;
+import org.springframework.stereotype.Repository;
 
-public interface ClientLinkDao extends AbstractDao<ClientLink> {
-    List<ClientLink> findByFacilityIdClientIdType(Integer facilityId, Integer clientId, Boolean currentlyLinked, ClientLink.Type type);
+@Repository
+public class ClientLinkDaoImpl extends AbstractDaoImpl<ClientLink> implements ClientLinkDao {
+	
+	public ClientLinkDaoImpl() {
+		super(ClientLink.class);
+	}
+
+	public List<ClientLink> findByFacilityIdClientIdType(Integer facilityId, Integer clientId, Boolean currentlyLinked, ClientLink.Type type) {
+		// build sql string
+		StringBuilder sqlCommand=new StringBuilder();
+		sqlCommand.append("select x from ClientLink x where x.facilityId=?1 and x.clientId=?2");
+		if (type!=null) sqlCommand.append(" and x.linkType=?3");
+		if (currentlyLinked!=null)
+		{
+			if (currentlyLinked) sqlCommand.append(" and x.unlinkProviderNo is null");
+			else sqlCommand.append(" and x.unlinkProviderNo is not null");
+		}
+		
+		// set parameters
+		Query query = entityManager.createQuery(sqlCommand.toString());
+		query.setParameter(1, facilityId);
+		query.setParameter(2, clientId);
+		if (type!=null) query.setParameter(3, type);
+		
+		// run query
+		@SuppressWarnings("unchecked")
+		List<ClientLink> results = query.getResultList();
+
+		return(results);
+	}
 }
