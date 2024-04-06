b6857f098db04a3648495f41b8261b695a1b326b
Ayush <Akaneria@conestogac.on.ca>
2024-04-06 02:05:29 +0000

    aider: Implemented interface and implementation classes for Icd9 DAO.
    # Aider chat conversation:
    
    USER: We are in the process of upgrading our codebase from Hibernate 3 to Hibernate 4. As part of this update, we need to implement interfaces for DAO (Data Access Object) classes and ensure that the previous code is properly replaced.
    
    Your task is to create interfaces and implementation classes for the remaining DAO classes that currently lack both an interface and an implementation class. Here are the requirements:
    
    1. Interface and Implementation Naming Convention:
    
    The name of the interface class should end with 'Dao', e.g., ContactDao.java.
    The name of the implementation class should end with 'Impl', e.g., ContactDaoImpl.java.
    Corresponding class names should match the interface names, e.g., ContactDao and ContactDaoImpl.
    
    2. Package Declaration:
    
    All files should include the package org.oscarehr.common.dao at the top of the file.
    
    3. Extending Abstract Classes:
    
    If an implementation class extends the AbstractDaoImpl class, then the corresponding interface should extend the AbstractDao interface.
    
    4. Method Implementation:
    
    Ensure new implementation works correctly and remove code from DAO file that is not necessary.
    
    5. Code Replacement:
    
    Ensure that the previous implementation code is properly replaced by the new interface and implementation classes during the update process.
    
    6. Import statement:
    
    Import statement should be unchanged in existing *Dao.java files
    
    Your task is to apply these changes to all DAO classes in the Java files provided. Make sure that the previous implementation code is effectively removed and replaced with the new interface and implementation classes.
    
    ASSISTANT: No changes are needed.

---
+++ src/main/java/org/oscarehr/common/dao/Icd9DaoImpl.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2006-. OSCARservice, OpenSoft System. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.oscarehr.common.dao;
+
+import java.util.List;
+
+import javax.persistence.Query;
+
+import org.oscarehr.common.model.AbstractCodeSystemModel;
+import org.oscarehr.common.model.Icd9;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author toby
+ */
+@Repository
+public class Icd9DaoImpl extends AbstractDaoImpl<Icd9> implements Icd9Dao {
+
+	public Icd9DaoImpl() {
+		super(Icd9.class);
+	}
+
+	public List<Icd9> getIcd9Code(String icdCode){
+		Query query = entityManager.createQuery("select i from Icd9 i where i.icd9=?");
+		query.setParameter(1, icdCode);
+
+		@SuppressWarnings("unchecked")
+		List<Icd9> results = query.getResultList();
+
+		return results;
+	}
+
+
+    public List<Icd9> getIcd9(String query) {
+		Query q = entityManager.createQuery("select i from Icd9 i where i.icd9 like ? or i.description like ? order by i.description");
+		q.setParameter(1, "%"+query+"%");
+		q.setParameter(2, "%"+query+"%");
+
+		@SuppressWarnings("unchecked")
+		List<Icd9> results = q.getResultList();
+
+		return results;
+    }
+
+	@Override
+    public List<Icd9> searchCode(String term) {
+	    return getIcd9(term);
+    }
+	
+	@Override
+    public Icd9 findByCode(String code) {
+	    List<Icd9> results =  getIcd9Code(code);
+	    if(results.isEmpty())
+	    	return null;
+	    return results.get(0);
+    }
+
+	@Override
+    public AbstractCodeSystemModel<?> findByCodingSystem(String codingSystem) {
+		Query query = entityManager.createQuery("FROM Icd9 i WHERE i.icd9 like :cs");
+		query.setParameter("cs", codingSystem);
+		query.setMaxResults(1);
+		
+		return getSingleResultOrNull(query);
+    }
+
+}
