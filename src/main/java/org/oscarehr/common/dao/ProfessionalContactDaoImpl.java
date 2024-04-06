56e5cf1d65f3a9bebfb103393c68882125f528c3
Ayush <Akaneria@conestogac.on.ca>
2024-04-06 02:49:16 +0000

    aider: Created interface and implementation for ProfessionalContactDao.
    # Aider chat conversation:
    
    USER: We are in the process of upgrading our codebase from Hibernate 3 to Hibernate 4. As part of this update, we need to implement interfaces for DAO (Data Access Object) classes and ensure that the previous code is properly replaced.
    
    Your task is to create interfaces and implementation classes for the remaining DAO classes that currently lack both an interface and an implementation class. Here are the requirements:
    
    1. Interface and Implementation Naming Convention:
    
    The name of the interface class should end with 'Dao', e.g., ContactDao.java.
    The name of the implementation class should end with 'Impl', e.g., ContactDaoImpl.java.
    Corresponding class names should match the interface names, e.g., ContactDao and ContactDaoImpl.
    
    2. Package Declaration:
    
    All files should include the package org.oscarehr.common.dao at the top of the file.
    
    3. Extending Abstract Classes:
    
    If an implementation class extends the AbstractDaoImpl class, then the corresponding interface should extend the AbstractDao interface.
    
    4. Method Implementation:
    
    Ensure new implementation works correctly and remove code from DAO file that is not necessary.
    
    5. Code Replacement:
    
    Ensure that the previous implementation code is properly replaced by the new interface and implementation classes during the update process.
    
    6. Import statement:
    
    Import statement should be unchanged in existing *Dao.java files
    
    Your task is to apply these changes to all DAO classes in the Java files provided. Make sure that the previous implementation code is effectively removed and replaced with the new interface and implementation classes.
    
    ASSISTANT: No changes are needed.

---
+++ src/main/java/org/oscarehr/common/dao/ProfessionalContactDaoImpl.java
@@ -0,0 +1,53 @@
+package org.oscarehr.common.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Query;
+import org.apache.commons.lang.StringEscapeUtils;
+import org.oscarehr.common.model.ProfessionalContact;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProfessionalContactDaoImpl extends AbstractDaoImpl<ProfessionalContact> implements ProfessionalContactDao {
+
+    public ProfessionalContactDaoImpl() {
+        super(ProfessionalContact.class);
+    }
+    
+    @Deprecated
+    @SuppressWarnings("unchecked")
+    public List<ProfessionalContact> findAll() {
+        Query query = createQuery("x", null);
+        return query.getResultList();
+    }
+    
+    public List<ProfessionalContact> search(String searchMode, String orderBy, String keyword) {
+        StringBuilder where = new StringBuilder();
+        List<String> paramList = new ArrayList<String>();
+        
+        if(searchMode.equals("search_name")) {
+            String[] temp = keyword.split("\\,\\p{Space}*");
+            if(temp.length>1) {
+              where.append("c.lastName like ?1 and c.firstName like ?2");
+              paramList.add(temp[0]+"%");
+              paramList.add(temp[1]+"%");
+            } else {
+              where.append("c.lastName like ?1");
+              paramList.add(temp[0]+"%");
+            }
+        }else {        
+            where.append("c." + StringEscapeUtils.escapeSql(searchMode) + " like ?1");
+            paramList.add(keyword+"%");
+        }            
+        String sql = "SELECT c from ProfessionalContact c where " + where.toString() + " order by " + orderBy;
+        
+        Query query = entityManager.createQuery(sql);
+        for(int x=0;x<paramList.size();x++) {
+            query.setParameter(x+1,paramList.get(x));
+        }        
+        
+        @SuppressWarnings("unchecked")
+        List<ProfessionalContact> contacts = query.getResultList();
+        return contacts;
+    }
+}
