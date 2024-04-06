7900f75636849f453db356f14a06fe214ca98660
Ayush <Akaneria@conestogac.on.ca>
2024-04-06 01:59:37 +0000

    aider: Implemented interface and implementation classes for HL7HandlerMSHMappingDao.
    # Aider chat conversation:
    
    USER: We are in the process of upgrading our codebase from Hibernate 3 to Hibernate 4. As part of this update, we need to implement interfaces for DAO (Data Access Object) classes and ensure that the previous code is properly replaced.
    
    Your task is to create interfaces and implementation classes for the remaining DAO classes that currently lack both an interface and an implementation class. Here are the requirements:
    
    1. Interface and Implementation Naming Convention:
    
    The name of the interface class should end with 'Dao', e.g., ContactDao.java.
    The name of the implementation class should end with 'Impl', e.g., ContactDaoImpl.java.
    Corresponding class names should match the interface names, e.g., ContactDao and ContactDaoImpl.
    
    2. Package Declaration:
    
    All files should include the package org.oscarehr.common.dao at the top of the file.
    
    3. Extending Abstract Classes:
    
    If an implementation class extends the AbstractDaoImpl class, then the corresponding interface should extend the AbstractDao interface.
    
    4. Method Implementation:
    
    Ensure new implementation works correctly and remove code from DAO file that is not necessary.
    
    5. Code Replacement:
    
    Ensure that the previous implementation code is properly replaced by the new interface and implementation classes during the update process.
    
    6. Import statement:
    
    Import statement should be unchanged in existing *Dao.java files
    
    Your task is to apply these changes to all DAO classes in the Java files provided. Make sure that the previous implementation code is effectively removed and replaced with the new interface and implementation classes.
    
    ASSISTANT: No changes are needed.

---
+++ src/main/java/org/oscarehr/common/dao/HL7HandlerMSHMappingDaoImpl.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2008-2012 Indivica Inc.
+ *
+ * This software is made available under the terms of the
+ * GNU General Public License, Version 2, 1991 (GPLv2).
+ * License details are available via "indivica.ca/gplv2"
+ * and "gnu.org/licenses/gpl-2.0.html".
+ */
+
+package org.oscarehr.common.dao;
+
+import javax.persistence.Query;
+
+import org.oscarehr.common.model.HL7HandlerMSHMapping;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class HL7HandlerMSHMappingDaoImpl extends AbstractDaoImpl<HL7HandlerMSHMapping> implements HL7HandlerMSHMappingDao {
+	
+	public HL7HandlerMSHMappingDaoImpl() {
+		super (HL7HandlerMSHMapping.class);
+	}
+	
+	@Override
+	public HL7HandlerMSHMapping findByFacility(String facility) {
+		String sql = "select x from HL7HandlerMSHMapping x where x.facility=?1";
+		Query query = entityManager.createQuery(sql);
+		query.setParameter(1, facility);
+		
+		return(getSingleResultOrNull(query));
+	}
+}
