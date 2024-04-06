93365c0f6d7a4059adecbeb607afc2c13a24640b
Ayush <Akaneria@conestogac.on.ca>
2024-04-06 02:32:30 +0000

    aider: Implemented interfaces and corresponding implementation classes for MeasurementTypeDao.
    # Aider chat conversation:
    
    USER: We are in the process of upgrading our codebase from Hibernate 3 to Hibernate 4. As part of this update, we need to implement interfaces for DAO (Data Access Object) classes and ensure that the previous code is properly replaced.
    
    Your task is to create interfaces and implementation classes for the remaining DAO classes that currently lack both an interface and an implementation class. Here are the requirements:
    
    1. Interface and Implementation Naming Convention:
    
    The name of the interface class should end with 'Dao', e.g., ContactDao.java.
    The name of the implementation class should end with 'Impl', e.g., ContactDaoImpl.java.
    Corresponding class names should match the interface names, e.g., ContactDao and ContactDaoImpl.
    
    2. Package Declaration:
    
    All files should include the package org.oscarehr.common.dao at the top of the file.
    
    3. Extending Abstract Classes:
    
    If an implementation class extends the AbstractDaoImpl class, then the corresponding interface should extend the AbstractDao interface.
    
    4. Method Implementation:
    
    Ensure new implementation works correctly and remove code from DAO file that is not necessary.
    
    5. Code Replacement:
    
    Ensure that the previous implementation code is properly replaced by the new interface and implementation classes during the update process.
    
    6. Import statement:
    
    Import statement should be unchanged in existing *Dao.java files
    
    Your task is to apply these changes to all DAO classes in the Java files provided. Make sure that the previous implementation code is effectively removed and replaced with the new interface and implementation classes.
    
    ASSISTANT: No changes are needed.

---
+++ src/main/java/org/oscarehr/common/dao/MeasurementTypeDaoImpl.java
@@ -0,0 +1,76 @@
+package org.oscarehr.common.dao;
+
+import java.util.List;
+import javax.persistence.Query;
+import org.oscarehr.common.model.MeasurementType;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@SuppressWarnings("unchecked")
+public class MeasurementTypeDaoImpl extends AbstractDaoImpl<MeasurementType> implements MeasurementTypeDao {
+
+    public MeasurementTypeDaoImpl() {
+        super(MeasurementType.class);
+    }
+
+    public List<MeasurementType> findAll() {
+        String sqlCommand = "select x from " + modelClass.getSimpleName() + " x order by x.type";
+        Query query = entityManager.createQuery(sqlCommand);
+        List<MeasurementType> results = query.getResultList();
+        return (results);
+    }
+
+    public List<MeasurementType> findAllOrderByName() {
+        String sqlCommand = "select x from " + modelClass.getSimpleName() + " x order by x.typeDisplayName";
+        Query query = entityManager.createQuery(sqlCommand);
+        List<MeasurementType> results = query.getResultList();
+        return (results);
+    }
+
+    public List<MeasurementType> findAllOrderById() {
+        String sqlCommand = "select x from " + modelClass.getSimpleName() + " x order by x.id";
+        Query query = entityManager.createQuery(sqlCommand);
+        List<MeasurementType> results = query.getResultList();
+        return (results);
+    }
+
+    public List<MeasurementType> findByType(String type) {
+        String sqlCommand = "select x from " + modelClass.getSimpleName()+" x where x.type=?1";
+        Query query = entityManager.createQuery(sqlCommand);
+        query.setParameter(1, type);
+        List<MeasurementType> results = query.getResultList();
+        return (results);
+    }
+
+    public List<MeasurementType> findByMeasuringInstructionAndTypeDisplayName(String measuringInstruction, String typeDisplayName) {
+        String sqlCommand = "select x from " + modelClass.getSimpleName()+" x where x.measuringInstruction=?1 AND x.typeDisplayName=?2";
+        Query query = entityManager.createQuery(sqlCommand);
+        query.setParameter(1, measuringInstruction);
+        query.setParameter(2, typeDisplayName);
+        List<MeasurementType> results = query.getResultList();
+        return (results);
+    }
+
+    public List<MeasurementType> findByTypeDisplayName(String typeDisplayName) {
+        String sqlCommand = "select x from " + modelClass.getSimpleName()+" x where x.typeDisplayName=?1";
+        Query query = entityManager.createQuery(sqlCommand);
+        query.setParameter(1, typeDisplayName);
+        List<MeasurementType> results = query.getResultList();
+        return (results);
+    }
+
+    public List<MeasurementType> findByTypeAndMeasuringInstruction(String type, String measuringInstruction) {
+        String sqlCommand = "select x from " + modelClass.getSimpleName()+" x where x.type=?1 AND x.measuringInstruction=?2 ";
+        Query query = entityManager.createQuery(sqlCommand);
+        query.setParameter(1, type);
+        query.setParameter(2, measuringInstruction);
+        List<MeasurementType> results = query.getResultList();
+        return (results);
+    }
+
+    public List<Object> findUniqueTypeDisplayNames() {
+        String sql = "SELECT DISTINCT m.typeDisplayName FROM MeasurementType m order by m.typeDisplayName";
+        Query query = entityManager.createQuery(sql);
+        return query.getResultList();
+    }
+}
