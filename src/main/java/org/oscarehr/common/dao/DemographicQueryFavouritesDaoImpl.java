9a678f7fdc7b4989969667ad93d171ae40c2fdb3
Ayush <Akaneria@conestogac.on.ca>
2024-04-06 01:24:45 +0000

    aider: Implemented interface and implementation classes for DemographicQueryFavouritesDao.
    # Aider chat conversation:
    
    USER: We are in the process of upgrading our codebase from Hibernate 3 to Hibernate 4. As part of this update, we need to implement interfaces for DAO (Data Access Object) classes and ensure that the previous code is properly replaced.
    
    Your task is to create interfaces and implementation classes for the remaining DAO classes that currently lack both an interface and an implementation class. Here are the requirements:
    
    1. Interface and Implementation Naming Convention:
    
    The name of the interface class should end with 'Dao', e.g., ContactDao.java.
    The name of the implementation class should end with 'Impl', e.g., ContactDaoImpl.java.
    Corresponding class names should match the interface names, e.g., ContactDao and ContactDaoImpl.
    
    2. Package Declaration:
    
    All files should include the package org.oscarehr.common.dao at the top of the file.
    
    3. Extending Abstract Classes:
    
    If an implementation class extends the AbstractDaoImpl class, then the corresponding interface should extend the AbstractDao interface.
    
    4. Method Implementation:
    
    Ensure new implementation works correctly and remove code from DAO file that is not necessary.
    
    5. Code Replacement:
    
    Ensure that the previous implementation code is properly replaced by the new interface and implementation classes during the update process.
    
    6. Import statement:
    
    Import statement should be unchanged in existing *Dao.java files
    
    Your task is to apply these changes to all DAO classes in the Java files provided. Make sure that the previous implementation code is effectively removed and replaced with the new interface and implementation classes.
    
    ASSISTANT: No changes are needed.

--- src/main/java/org/oscarehr/common/dao/DemographicQueryFavouritesDaoImpl.java
+++ src/main/java/org/oscarehr/common/dao/DemographicQueryFavouritesDaoImpl.java
@@ -25,8 +25,24 @@
 package org.oscarehr.common.dao;
 
 import java.util.List;
+import javax.persistence.Query;
 import org.oscarehr.common.model.DemographicQueryFavourite;
+import org.springframework.stereotype.Repository;
 
-public interface DemographicQueryFavouritesDao extends AbstractDao<DemographicQueryFavourite> {
-    List<DemographicQueryFavourite> findByArchived(String archived);
+@Repository
+public class DemographicQueryFavouritesDaoImpl extends AbstractDaoImpl<DemographicQueryFavourite> implements DemographicQueryFavouritesDao {
+
+	public DemographicQueryFavouritesDaoImpl() {
+		super(DemographicQueryFavourite.class);
+	}
+
+	@Override
+	public List<DemographicQueryFavourite> findByArchived(String archived) {
+		String sql = "select x from DemographicQueryFavourite x where x.archived=? order by x.queryName";
+		Query query = entityManager.createQuery(sql);
+		query.setParameter(1, archived);
+		@SuppressWarnings("unchecked")
+		List<DemographicQueryFavourite> results = query.getResultList();
+		return results;
+	}
 }
