748612394cb7195fccdc87273a71c50f6b7d32f8
Ayush <Akaneria@conestogac.on.ca>
2024-04-06 01:23:24 +0000

    aider: Implemented interface and implementation classes for DemographicAccessoryDao.
    # Aider chat conversation:
    
    USER: We are in the process of upgrading our codebase from Hibernate 3 to Hibernate 4. As part of this update, we need to implement interfaces for DAO (Data Access Object) classes and ensure that the previous code is properly replaced.
    
    Your task is to create interfaces and implementation classes for the remaining DAO classes that currently lack both an interface and an implementation class. Here are the requirements:
    
    1. Interface and Implementation Naming Convention:
    
    The name of the interface class should end with 'Dao', e.g., ContactDao.java.
    The name of the implementation class should end with 'Impl', e.g., ContactDaoImpl.java.
    Corresponding class names should match the interface names, e.g., ContactDao and ContactDaoImpl.
    
    2. Package Declaration:
    
    All files should include the package org.oscarehr.common.dao at the top of the file.
    
    3. Extending Abstract Classes:
    
    If an implementation class extends the AbstractDaoImpl class, then the corresponding interface should extend the AbstractDao interface.
    
    4. Method Implementation:
    
    Ensure new implementation works correctly and remove code from DAO file that is not necessary.
    
    5. Code Replacement:
    
    Ensure that the previous implementation code is properly replaced by the new interface and implementation classes during the update process.
    
    6. Import statement:
    
    Import statement should be unchanged in existing *Dao.java files
    
    Your task is to apply these changes to all DAO classes in the Java files provided. Make sure that the previous implementation code is effectively removed and replaced with the new interface and implementation classes.
    
    ASSISTANT: No changes are needed.

--- src/main/java/org/oscarehr/common/dao/DemographicAccessoryDaoImpl.java
+++ src/main/java/org/oscarehr/common/dao/DemographicAccessoryDaoImpl.java
@@ -24,8 +24,26 @@
 
 package org.oscarehr.common.dao;
 
-import org.oscarehr.common.model.DemographicAccessory;
+import javax.persistence.Query;
 
-public interface DemographicAccessoryDao extends AbstractDao<DemographicAccessory> {
-    long findCount(Integer demographicNo);
+import org.oscarehr.common.model.DemographicAccessory;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DemographicAccessoryDaoImpl extends AbstractDaoImpl<DemographicAccessory> implements DemographicAccessoryDao {
+
+	public DemographicAccessoryDaoImpl() {
+		super(DemographicAccessory.class);
+	}
+
+    @Override
+    public long findCount(Integer demographicNo) {
+    	String sql = "select count(x) from DemographicAccessory x where x.demographicNo=?1";
+    	Query query = entityManager.createQuery(sql);
+    	query.setParameter(1,demographicNo);
+
+        Long count = (Long)query.getSingleResult();
+
+        return count;
+    }
 }
