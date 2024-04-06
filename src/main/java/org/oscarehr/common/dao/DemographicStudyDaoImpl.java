6cbdf6a885e9bc9bb7ded1803395ce65b221fd73
Ayush <Akaneria@conestogac.on.ca>
2024-04-06 01:25:17 +0000

    aider: Implemented interface and implementation class for DemographicStudyDao.
    # Aider chat conversation:
    
    USER: We are in the process of upgrading our codebase from Hibernate 3 to Hibernate 4. As part of this update, we need to implement interfaces for DAO (Data Access Object) classes and ensure that the previous code is properly replaced.
    
    Your task is to create interfaces and implementation classes for the remaining DAO classes that currently lack both an interface and an implementation class. Here are the requirements:
    
    1. Interface and Implementation Naming Convention:
    
    The name of the interface class should end with 'Dao', e.g., ContactDao.java.
    The name of the implementation class should end with 'Impl', e.g., ContactDaoImpl.java.
    Corresponding class names should match the interface names, e.g., ContactDao and ContactDaoImpl.
    
    2. Package Declaration:
    
    All files should include the package org.oscarehr.common.dao at the top of the file.
    
    3. Extending Abstract Classes:
    
    If an implementation class extends the AbstractDaoImpl class, then the corresponding interface should extend the AbstractDao interface.
    
    4. Method Implementation:
    
    Ensure new implementation works correctly and remove code from DAO file that is not necessary.
    
    5. Code Replacement:
    
    Ensure that the previous implementation code is properly replaced by the new interface and implementation classes during the update process.
    
    6. Import statement:
    
    Import statement should be unchanged in existing *Dao.java files
    
    Your task is to apply these changes to all DAO classes in the Java files provided. Make sure that the previous implementation code is effectively removed and replaced with the new interface and implementation classes.
    
    ASSISTANT: No changes are needed.

---
+++ src/main/java/org/oscarehr/common/dao/DemographicStudyDaoImpl.java
@@ -0,0 +1,50 @@
+package org.oscarehr.common.dao;
+
+import org.oscarehr.common.model.DemographicStudy;
+import org.oscarehr.common.model.DemographicStudyPK;
+
+import javax.persistence.Query;
+import java.util.List;
+
+public class DemographicStudyDaoImpl extends AbstractDaoImpl<DemographicStudy> implements DemographicStudyDao {
+
+    public DemographicStudyDaoImpl() {
+        super(DemographicStudy.class);
+    }
+
+    @Override
+    public List<DemographicStudy> findAll() {
+        Query query = createQuery("x", null);
+        return query.getResultList();
+    }
+
+    @Override
+    public int removeByDemographicNo(Integer demographicNo) {
+        Query query = entityManager.createQuery("delete from DemographicStudy x where x.id.demographicNo=?");
+        query.setParameter(1, demographicNo);
+        return query.executeUpdate();
+    }
+
+    @Override
+    public DemographicStudy findByDemographicNoAndStudyNo(int demographicNo, int studyNo) {
+        DemographicStudyPK pk = new DemographicStudyPK();
+        pk.setDemographicNo(demographicNo);
+        pk.setStudyNo(studyNo);
+
+        return find(pk);
+    }
+
+    @Override
+    public List<DemographicStudy> findByStudyNo(int studyNo) {
+        Query query = entityManager.createQuery("select x from DemographicStudy x where x.id.studyNo=?");
+        query.setParameter(1, studyNo);
+        return query.getResultList();
+    }
+
+    @Override
+    public List<DemographicStudy> findByDemographicNo(int demographicNo) {
+        Query query = entityManager.createQuery("select x from DemographicStudy x where x.id.demographicNo=?");
+        query.setParameter(1, demographicNo);
+        return query.getResultList();
+    }
+}
