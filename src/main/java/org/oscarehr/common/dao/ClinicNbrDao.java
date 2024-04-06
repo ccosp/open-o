162082f782518ff471e99da9f8f64e56a15fa757
Ayush <Akaneria@conestogac.on.ca>
2024-04-06 01:05:08 +0000

    aider: Implemented ClinicNbrDao interface and ClinicNbrDaoImpl class.
    # Aider chat conversation:
    
    USER: We are in the process of upgrading our codebase from Hibernate 3 to Hibernate 4. As part of this update, we need to implement interfaces for DAO (Data Access Object) classes and ensure that the previous code is properly replaced.
    
    Your task is to create interfaces and implementation classes for the remaining DAO classes that currently lack both an interface and an implementation class. Here are the requirements:
    
    1. Interface and Implementation Naming Convention:
    
    The name of the interface class should end with 'Dao', e.g., ContactDao.java.
    The name of the implementation class should end with 'Impl', e.g., ContactDaoImpl.java.
    Corresponding class names should match the interface names, e.g., ContactDao and ContactDaoImpl.
    
    2. Package Declaration:
    
    All files should include the package org.oscarehr.common.dao at the top of the file.
    
    3. Extending Abstract Classes:
    
    If an implementation class extends the AbstractDaoImpl class, then the corresponding interface should extend the AbstractDao interface.
    
    4. Method Implementation:
    
    Ensure new implementation works correctly and remove code from DAO file that is not necessary.
    
    5. Code Replacement:
    
    Ensure that the previous implementation code is properly replaced by the new interface and implementation classes during the update process.
    
    6. Import statement:
    
    Import statement should be unchanged in existing *Dao.java files
    
    Your task is to apply these changes to all DAO classes in the Java files provided. Make sure that the previous implementation code is effectively removed and replaced with the new interface and implementation classes.
    
    ASSISTANT: No changes are needed.

--- src/main/java/org/oscarehr/common/dao/ClinicNbrDao.java
+++ src/main/java/org/oscarehr/common/dao/ClinicNbrDao.java
@@ -11,45 +11,13 @@ package org.oscarehr.common.dao;
 
 import java.util.ArrayList;
 
-import javax.persistence.Query;
-
 import org.oscarehr.common.model.ClinicNbr;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public class ClinicNbrDao extends AbstractDaoImpl<ClinicNbr> {
+public interface ClinicNbrDao extends AbstractDao<ClinicNbr> {
 	
-	public ClinicNbrDao() {
-		super(ClinicNbr.class);
-	}
+	ArrayList<ClinicNbr> findAll();
 	
-	public ArrayList<ClinicNbr> findAll() {
-		Query query = entityManager.createQuery("select x from "+modelClass.getSimpleName()+" x where nbr_status != 'D' order by nbr_value asc");
-		@SuppressWarnings("unchecked")
-		ArrayList<ClinicNbr> results= new ArrayList<ClinicNbr>(query.getResultList());
-		return(results);
-	}	
+	Integer removeEntry(Integer id);
 	
-	public Integer removeEntry(Integer id) {
-		try {
-			ClinicNbr clinicNbr = find(id);
-			clinicNbr.setNbrStatus("D");
-			merge(clinicNbr);
-			return id;
-		} catch (Exception e) {
-			return 0;
-		}
-	}
-	
-	public int addEntry(String nbrValue, String nbrString) {
-		try {
-			ClinicNbr clinicNbr = new ClinicNbr();
-			clinicNbr.setNbrValue(nbrValue);
-			clinicNbr.setNbrString(nbrString);
-			persist(clinicNbr);
-			return clinicNbr.getId();
-		} catch (Exception e) {
-			return 0;
-		}
-	}	
+	int addEntry(String nbrValue, String nbrString);
 }
