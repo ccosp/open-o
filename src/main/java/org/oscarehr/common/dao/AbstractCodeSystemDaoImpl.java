ea6fc1defd2a81e1cd11cb682657fb713b3f3f04
Ayush <Akaneria@conestogac.on.ca>
2024-04-06 00:39:32 +0000

    aider: Created interfaces and implementation classes for AbstractCodeSystemDao and AbstractCodeSystemDaoImpl.
    # Aider chat conversation:
    
    USER: We are in the process of upgrading our codebase from Hibernate 3 to Hibernate 4. As part of this update, we need to implement interfaces for DAO (Data Access Object) classes and ensure that the previous code is properly replaced.
    
    Your task is to create interfaces and implementation classes for the remaining DAO classes that currently lack both an interface and an implementation class. Here are the requirements:
    
    1. Interface and Implementation Naming Convention:
    
    The name of the interface class should end with 'Dao', e.g., ContactDao.java.
    The name of the implementation class should end with 'Impl', e.g., ContactDaoImpl.java.
    Corresponding class names should match the interface names, e.g., ContactDao and ContactDaoImpl.
    
    2. Package Declaration:
    
    All files should include the package org.oscarehr.common.dao at the top of the file.
    
    3. Extending Abstract Classes:
    
    If an implementation class extends the AbstractDaoImpl class, then the corresponding interface should extend the AbstractDao interface.
    
    4. Method Implementation:
    
    Ensure new implementation works correctly and remove code from DAO file that is not necessary.
    
    5. Code Replacement:
    
    Ensure that the previous implementation code is properly replaced by the new interface and implementation classes during the update process.
    
    6. Import statement:
    
    Import statement should be unchanged in existing *Dao.java files
    
    Your task is to apply these changes to all DAO classes in the Java files provided. Make sure that the previous implementation code is effectively removed and replaced with the new interface and implementation classes.
    
    ASSISTANT: No changes are needed.

---
+++ src/main/java/org/oscarehr/common/dao/AbstractCodeSystemDaoImpl.java
@@ -0,0 +1,34 @@
+package org.oscarehr.common.dao;
+
+import java.util.List;
+import org.oscarehr.common.model.AbstractCodeSystemModel;
+
+public abstract class AbstractCodeSystemDaoImpl<T extends AbstractCodeSystemModel<?>> extends AbstractDaoImpl<T> implements AbstractCodeSystemDao<T> {
+
+    public static enum codingSystem {icd9,icd10,ichppccode,msp,SnomedCore}
+
+    public static String getDaoName(codingSystem codeSystem) {
+        String object;
+        switch(codeSystem) {
+        case SnomedCore: object = "snomedCoreDao";
+            break;
+        case icd10: object = "icd10Dao";
+            break;
+        case icd9: object = "icd9Dao";
+            break;
+        case ichppccode: object = "ichppccodeDao";
+            break;
+        case msp: object = "diagnosticCodeDao";
+            break;
+        default: throw new IllegalArgumentException("Unsupported code system: " + codeSystem + ". Please use one of icd9, ichppccode, snomedcore");                
+        }       
+        return object;
+    }
+
+    public abstract List<T> searchCode(String term);
+
+    public abstract T findByCode(String code);
+
+    public abstract AbstractCodeSystemModel<?> findByCodingSystem(String codingSystem);
+
+}
