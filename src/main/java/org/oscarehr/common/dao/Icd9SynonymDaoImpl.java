0371df4e9be63705a1df9d369bcf468b9999bc18
Ayush <Akaneria@conestogac.on.ca>
2024-04-06 02:06:04 +0000

    aider: Implemented interfaces and implementation classes for Icd9SynonymDao.
    # Aider chat conversation:
    
    USER: We are in the process of upgrading our codebase from Hibernate 3 to Hibernate 4. As part of this update, we need to implement interfaces for DAO (Data Access Object) classes and ensure that the previous code is properly replaced.
    
    Your task is to create interfaces and implementation classes for the remaining DAO classes that currently lack both an interface and an implementation class. Here are the requirements:
    
    1. Interface and Implementation Naming Convention:
    
    The name of the interface class should end with 'Dao', e.g., ContactDao.java.
    The name of the implementation class should end with 'Impl', e.g., ContactDaoImpl.java.
    Corresponding class names should match the interface names, e.g., ContactDao and ContactDaoImpl.
    
    2. Package Declaration:
    
    All files should include the package org.oscarehr.common.dao at the top of the file.
    
    3. Extending Abstract Classes:
    
    If an implementation class extends the AbstractDaoImpl class, then the corresponding interface should extend the AbstractDao interface.
    
    4. Method Implementation:
    
    Ensure new implementation works correctly and remove code from DAO file that is not necessary.
    
    5. Code Replacement:
    
    Ensure that the previous implementation code is properly replaced by the new interface and implementation classes during the update process.
    
    6. Import statement:
    
    Import statement should be unchanged in existing *Dao.java files
    
    Your task is to apply these changes to all DAO classes in the Java files provided. Make sure that the previous implementation code is effectively removed and replaced with the new interface and implementation classes.
    
    ASSISTANT: No changes are needed.

--- src/main/java/org/oscarehr/common/dao/Icd9SynonymDaoImpl.java
+++ src/main/java/org/oscarehr/common/dao/Icd9SynonymDaoImpl.java
@@ -23,8 +23,29 @@
  */
 package org.oscarehr.common.dao;
 
+import javax.persistence.Query;
 import org.oscarehr.common.model.Icd9Synonym;
+import org.springframework.stereotype.Repository;
+/*
+ * Author: Dennis Warren 
+ * Company: Colcamex Resources
+ * Date: November 2014
+ * For: UBC Pharmacy Clinic and McMaster Department of Family Medicine
+ */
+@Repository
+public class Icd9SynonymDaoImpl extends AbstractDaoImpl<Icd9Synonym> implements Icd9SynonymDao {
 
-public interface Icd9SynonymDao extends AbstractDao<Icd9Synonym> {
-    Icd9Synonym findPatientFriendlyTranslationFor(String dxCode);
+    public Icd9SynonymDaoImpl() {
+        super(Icd9Synonym.class);
+    }
+    
+    @Override
+    public Icd9Synonym findPatientFriendlyTranslationFor(String dxCode) {
+        String queryString = "SELECT x FROM Icd9Synonym x WHERE x.dxCode LIKE ?1";
+        Query query = entityManager.createQuery(queryString);
+        query.setParameter(1, dxCode);
+
+        return super.getSingleResultOrNull(query);
+    }
+
 }
