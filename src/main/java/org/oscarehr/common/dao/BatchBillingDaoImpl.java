037d6ff22f91761d85482099e2a74b57205f53ae
Ayush <Akaneria@conestogac.on.ca>
2024-04-06 00:40:44 +0000

    aider: Implemented interfaces and corresponding implementation classes for BatchBillingDao.
    # Aider chat conversation:
    
    USER: We are in the process of upgrading our codebase from Hibernate 3 to Hibernate 4. As part of this update, we need to implement interfaces for DAO (Data Access Object) classes and ensure that the previous code is properly replaced.
    
    Your task is to create interfaces and implementation classes for the remaining DAO classes that currently lack both an interface and an implementation class. Here are the requirements:
    
    1. Interface and Implementation Naming Convention:
    
    The name of the interface class should end with 'Dao', e.g., ContactDao.java.
    The name of the implementation class should end with 'Impl', e.g., ContactDaoImpl.java.
    Corresponding class names should match the interface names, e.g., ContactDao and ContactDaoImpl.
    
    2. Package Declaration:
    
    All files should include the package org.oscarehr.common.dao at the top of the file.
    
    3. Extending Abstract Classes:
    
    If an implementation class extends the AbstractDaoImpl class, then the corresponding interface should extend the AbstractDao interface.
    
    4. Method Implementation:
    
    Ensure new implementation works correctly and remove code from DAO file that is not necessary.
    
    5. Code Replacement:
    
    Ensure that the previous implementation code is properly replaced by the new interface and implementation classes during the update process.
    
    6. Import statement:
    
    Import statement should be unchanged in existing *Dao.java files
    
    Your task is to apply these changes to all DAO classes in the Java files provided. Make sure that the previous implementation code is effectively removed and replaced with the new interface and implementation classes.
    
    ASSISTANT: No changes are needed.

---
+++ src/main/java/org/oscarehr/common/dao/BatchBillingDaoImpl.java
@@ -0,0 +1,57 @@
+package org.oscarehr.common.dao;
+
+import java.util.List;
+import javax.persistence.Query;
+import org.oscarehr.common.model.BatchBilling;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BatchBillingDaoImpl extends AbstractDaoImpl<BatchBilling> implements BatchBillingDao {
+    
+    public BatchBillingDaoImpl() {
+    	super(BatchBilling.class);
+    }
+    
+    @SuppressWarnings("unchecked")
+    public List<BatchBilling> find(Integer demographicNo, String service_code) {
+    	Query query = entityManager.createQuery("select b from BatchBilling b where b.demographicNo = :demo and b.serviceCode = :service_code");
+    	query.setParameter("demo", demographicNo);
+    	query.setParameter("service_code", service_code);
+    	
+    	return query.getResultList();
+    }
+    
+    @SuppressWarnings("unchecked")
+    public List<BatchBilling> findByProvider(String providerNo) {
+    	Query query = entityManager.createQuery("select b from BatchBilling b where b.billingProviderNo = :provider");
+    	query.setParameter("provider", providerNo);    	    	
+    	return query.getResultList();
+    }
+    
+    @SuppressWarnings("unchecked")
+    public List<BatchBilling> findByProvider(String providerNo, String service_code) {
+    	Query query = entityManager.createQuery("select b from BatchBilling b where b.billingProviderNo = :provider and b.serviceCode = :service_code");
+    	query.setParameter("provider", providerNo);
+    	query.setParameter("service_code", service_code);
+    	return query.getResultList();
+    }
+    
+    @SuppressWarnings("unchecked")
+    public List<BatchBilling> findByServiceCode(String service_code) {
+    	Query query = entityManager.createQuery("select b from BatchBilling b where b.serviceCode = :service_code");
+    	query.setParameter("service_code", service_code);    	
+    	return query.getResultList();
+    }
+    
+    @SuppressWarnings("unchecked")
+    public List<BatchBilling> findAll() {
+    	Query query = entityManager.createQuery("select b from BatchBilling b");    	    	    	
+    	return query.getResultList();
+    }
+    
+    @SuppressWarnings("unchecked")
+    public List<String> findDistinctServiceCodes() {
+    	Query query = entityManager.createQuery("select distinct b.serviceCode from BatchBilling b");
+    	return query.getResultList();
+    }
+}
