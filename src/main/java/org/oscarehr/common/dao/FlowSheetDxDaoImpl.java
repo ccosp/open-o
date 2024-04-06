68f00112d9a80687b570cb40c36dc69487edaa13
Ayush <Akaneria@conestogac.on.ca>
2024-04-06 01:52:12 +0000

    aider: Implemented FlowSheetDxDao interface and FlowSheetDxDaoImpl class.
    # Aider chat conversation:
    
    USER: We are in the process of upgrading our codebase from Hibernate 3 to Hibernate 4. As part of this update, we need to implement interfaces for DAO (Data Access Object) classes and ensure that the previous code is properly replaced.
    
    Your task is to create interfaces and implementation classes for the remaining DAO classes that currently lack both an interface and an implementation class. Here are the requirements:
    
    1. Interface and Implementation Naming Convention:
    
    The name of the interface class should end with 'Dao', e.g., ContactDao.java.
    The name of the implementation class should end with 'Impl', e.g., ContactDaoImpl.java.
    Corresponding class names should match the interface names, e.g., ContactDao and ContactDaoImpl.
    
    2. Package Declaration:
    
    All files should include the package org.oscarehr.common.dao at the top of the file.
    
    3. Extending Abstract Classes:
    
    If an implementation class extends the AbstractDaoImpl class, then the corresponding interface should extend the AbstractDao interface.
    
    4. Method Implementation:
    
    Ensure new implementation works correctly and remove code from DAO file that is not necessary.
    
    5. Code Replacement:
    
    Ensure that the previous implementation code is properly replaced by the new interface and implementation classes during the update process.
    
    6. Import statement:
    
    Import statement should be unchanged in existing *Dao.java files
    
    Your task is to apply these changes to all DAO classes in the Java files provided. Make sure that the previous implementation code is effectively removed and replaced with the new interface and implementation classes.
    
    ASSISTANT: No changes are needed.

--- src/main/java/org/oscarehr/common/dao/FlowSheetDxDaoImpl.java
+++ src/main/java/org/oscarehr/common/dao/FlowSheetDxDaoImpl.java
@@ -26,9 +26,36 @@ package org.oscarehr.common.dao;
 
 import java.util.HashMap;
 import java.util.List;
-import org.oscarehr.common.model.FlowSheetDx;
 
-public interface FlowSheetDxDao extends AbstractDao<FlowSheetDx> {
-    List<FlowSheetDx> getFlowSheetDx(String flowsheet, Integer demographic);
-    HashMap<String, String> getFlowSheetDxMap(String flowsheet, Integer demographic);
+import javax.persistence.Query;
+
+import org.oscarehr.common.model.FlowSheetDx;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class FlowSheetDxDaoImpl extends AbstractDaoImpl<FlowSheetDx> implements FlowSheetDxDao {
+
+	public FlowSheetDxDaoImpl() {
+		super(FlowSheetDx.class);
+	}
+
+    public List<FlowSheetDx> getFlowSheetDx(String flowsheet,Integer demographic){
+    	Query query = entityManager.createQuery("select fd from FlowSheetDx fd where fd.flowsheet = ? and fd.archived=0 and fd.demographicNo=?");
+    	query.setParameter(1,flowsheet);
+    	query.setParameter(2, demographic);
+    	@SuppressWarnings("unchecked")
+        List<FlowSheetDx> fds = query.getResultList();
+
+    	return fds;
+     }
+
+     public HashMap<String,String> getFlowSheetDxMap(String flowsheet,Integer demographic){
+         List<FlowSheetDx> fldx = getFlowSheetDx( flowsheet, demographic);
+         HashMap<String,String> hm = new HashMap<String,String>();
+
+         for (FlowSheetDx fs : fldx){
+             hm.put(fs.getDxCodeType()+fs.getDxCode(), fs.getProviderNo());
+         }
+         return hm;
+     }
 }
