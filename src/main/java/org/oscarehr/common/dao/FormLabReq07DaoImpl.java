587d6b1d8e31137f2e284d3bee37d8727a78462d
Ayush <Akaneria@conestogac.on.ca>
2024-04-06 01:56:10 +0000

    aider: Implemented FormLabReq07Dao interface and FormLabReq07DaoImpl class.
    # Aider chat conversation:
    
    USER: We are in the process of upgrading our codebase from Hibernate 3 to Hibernate 4. As part of this update, we need to implement interfaces for DAO (Data Access Object) classes and ensure that the previous code is properly replaced.
    
    Your task is to create interfaces and implementation classes for the remaining DAO classes that currently lack both an interface and an implementation class. Here are the requirements:
    
    1. Interface and Implementation Naming Convention:
    
    The name of the interface class should end with 'Dao', e.g., ContactDao.java.
    The name of the implementation class should end with 'Impl', e.g., ContactDaoImpl.java.
    Corresponding class names should match the interface names, e.g., ContactDao and ContactDaoImpl.
    
    2. Package Declaration:
    
    All files should include the package org.oscarehr.common.dao at the top of the file.
    
    3. Extending Abstract Classes:
    
    If an implementation class extends the AbstractDaoImpl class, then the corresponding interface should extend the AbstractDao interface.
    
    4. Method Implementation:
    
    Ensure new implementation works correctly and remove code from DAO file that is not necessary.
    
    5. Code Replacement:
    
    Ensure that the previous implementation code is properly replaced by the new interface and implementation classes during the update process.
    
    6. Import statement:
    
    Import statement should be unchanged in existing *Dao.java files
    
    Your task is to apply these changes to all DAO classes in the Java files provided. Make sure that the previous implementation code is effectively removed and replaced with the new interface and implementation classes.
    
    ASSISTANT: No changes are needed.

--- src/main/java/org/oscarehr/common/dao/FormLabReq07DaoImpl.java
+++ src/main/java/org/oscarehr/common/dao/FormLabReq07DaoImpl.java
@@ -24,9 +24,34 @@
 package org.oscarehr.common.dao;
 
 import java.util.List;
+import javax.persistence.Query;
 import org.oscarehr.common.model.FormLabReq07;
+import org.springframework.stereotype.Repository;
 
-public interface FormLabReq07Dao extends AbstractDao<FormLabReq07> {
-	List<FormLabReq07> findCreatinine();
-	List<FormLabReq07> findAcr();
+@Repository
+public class FormLabReq07DaoImpl extends AbstractDaoImpl<FormLabReq07> implements FormLabReq07Dao {
+
+	public FormLabReq07DaoImpl() {
+		super(FormLabReq07.class);
+	}
+	
+	public List<FormLabReq07> findCreatinine() {
+		Query q = entityManager.createQuery("select f from FormLabReq07 f where f.creatinine=?");
+		q.setParameter(1, true);
+		
+		@SuppressWarnings("unchecked")
+		List<FormLabReq07> results = q.getResultList();
+		
+		return results;
+	}
+	
+	public List<FormLabReq07> findAcr() {
+		Query q = entityManager.createQuery("select f from FormLabReq07 f where f.acr=?");
+		q.setParameter(1, true);
+		
+		@SuppressWarnings("unchecked")
+		List<FormLabReq07> results = q.getResultList();
+		
+		return results;
+	}
 }
