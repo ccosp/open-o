e7354fbfafc38c3da7155692a25c6b9bb459aa01
Ayush <Akaneria@conestogac.on.ca>
2024-04-06 02:45:09 +0000

    aider: Implemented interface and implementation class for OtherIdDao.
    # Aider chat conversation:
    
    USER: We are in the process of upgrading our codebase from Hibernate 3 to Hibernate 4. As part of this update, we need to implement interfaces for DAO (Data Access Object) classes and ensure that the previous code is properly replaced.
    
    Your task is to create interfaces and implementation classes for the remaining DAO classes that currently lack both an interface and an implementation class. Here are the requirements:
    
    1. Interface and Implementation Naming Convention:
    
    The name of the interface class should end with 'Dao', e.g., ContactDao.java.
    The name of the implementation class should end with 'Impl', e.g., ContactDaoImpl.java.
    Corresponding class names should match the interface names, e.g., ContactDao and ContactDaoImpl.
    
    2. Package Declaration:
    
    All files should include the package org.oscarehr.common.dao at the top of the file.
    
    3. Extending Abstract Classes:
    
    If an implementation class extends the AbstractDaoImpl class, then the corresponding interface should extend the AbstractDao interface.
    
    4. Method Implementation:
    
    Ensure new implementation works correctly and remove code from DAO file that is not necessary.
    
    5. Code Replacement:
    
    Ensure that the previous implementation code is properly replaced by the new interface and implementation classes during the update process.
    
    6. Import statement:
    
    Import statement should be unchanged in existing *Dao.java files
    
    Your task is to apply these changes to all DAO classes in the Java files provided. Make sure that the previous implementation code is effectively removed and replaced with the new interface and implementation classes.
    
    ASSISTANT: No changes are needed.

--- src/main/java/org/oscarehr/common/dao/OtherIdDaoImpl.java
+++ src/main/java/org/oscarehr/common/dao/OtherIdDaoImpl.java
@@ -6,47 +6,4 @@ import org.oscarehr.common.model.OtherId;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class OtherIdDaoImpl extends AbstractDaoImpl<OtherId> implements OtherIdDao {
-
-    public OtherIdDaoImpl() {
-        super(OtherId.class);
-    }
-
-    public OtherId getOtherId(Integer tableName, Integer tableId, String otherKey){
-        return getOtherId(tableName,String.valueOf(tableId),otherKey);
-    }
-
-    public OtherId getOtherId(Integer tableName, String tableId, String otherKey){
-        Query query = entityManager.createQuery("select o from OtherId o where o.tableName=? and o.tableId=? and o.otherKey=? and o.deleted=? order by o.id desc");
-        query.setParameter(1, tableName);
-        query.setParameter(2, tableId);
-        query.setParameter(3, otherKey);
-        query.setParameter(4, false);
-
-        @SuppressWarnings("unchecked")
-        List<OtherId> otherIdList = query.getResultList();
-
-        return otherIdList.size()>0 ? otherIdList.get(0) : null;
-    }
-
-    public OtherId searchTable(Integer tableName, String otherKey, String otherValue){
-        Query query = entityManager.createQuery("select o from OtherId o where o.tableName=? and o.otherKey=? and o.otherId=? and o.deleted=? order by o.id desc");
-        query.setParameter(1, tableName);
-        query.setParameter(2, otherKey);
-        query.setParameter(3, otherValue);
-        query.setParameter(4, false);
-
-        @SuppressWarnings("unchecked")
-        List<OtherId> otherIdList = query.getResultList();
-
-        return otherIdList.size()>0 ? otherIdList.get(0) : null;
-    }
-
-    public void save(OtherId otherId) {
-        if(otherId.getId() != null && otherId.getId().intValue() > 0 ) {
-            merge(otherId);
-        } else {
-            persist(otherId);
-        }
-    }
-}
+public class OtherIdDaoImpl extends
