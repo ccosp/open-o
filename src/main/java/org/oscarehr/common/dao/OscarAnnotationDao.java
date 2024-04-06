582c774869392fdcbcfe31b89e73a68f85f284de
Ayush <Akaneria@conestogac.on.ca>
2024-04-06 02:42:52 +0000

    aider: Implemented interface and implementation class for OscarAnnotationDao.
    # Aider chat conversation:
    
    USER: We are in the process of upgrading our codebase from Hibernate 3 to Hibernate 4. As part of this update, we need to implement interfaces for DAO (Data Access Object) classes and ensure that the previous code is properly replaced.
    
    Your task is to create interfaces and implementation classes for the remaining DAO classes that currently lack both an interface and an implementation class. Here are the requirements:
    
    1. Interface and Implementation Naming Convention:
    
    The name of the interface class should end with 'Dao', e.g., ContactDao.java.
    The name of the implementation class should end with 'Impl', e.g., ContactDaoImpl.java.
    Corresponding class names should match the interface names, e.g., ContactDao and ContactDaoImpl.
    
    2. Package Declaration:
    
    All files should include the package org.oscarehr.common.dao at the top of the file.
    
    3. Extending Abstract Classes:
    
    If an implementation class extends the AbstractDaoImpl class, then the corresponding interface should extend the AbstractDao interface.
    
    4. Method Implementation:
    
    Ensure new implementation works correctly and remove code from DAO file that is not necessary.
    
    5. Code Replacement:
    
    Ensure that the previous implementation code is properly replaced by the new interface and implementation classes during the update process.
    
    6. Import statement:
    
    Import statement should be unchanged in existing *Dao.java files
    
    Your task is to apply these changes to all DAO classes in the Java files provided. Make sure that the previous implementation code is effectively removed and replaced with the new interface and implementation classes.
    
    ASSISTANT: No changes are needed.

--- src/main/java/org/oscarehr/common/dao/OscarAnnotationDao.java
+++ src/main/java/org/oscarehr/common/dao/OscarAnnotationDao.java
@@ -22,57 +22,12 @@
  * Ontario, Canada
  */
 
-
 package org.oscarehr.common.dao;
 
-import java.util.List;
-import java.util.UUID;
-
-import javax.persistence.Query;
-
 import org.oscarehr.common.model.OscarAnnotation;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public class OscarAnnotationDao extends AbstractDaoImpl<OscarAnnotation>{
-
-	public OscarAnnotationDao() {
-		super(OscarAnnotation.class);
-	}
-
-    public OscarAnnotation getAnnotations(String demoNo, String tableName,Long tableId){
-    	Query query = entityManager.createQuery("select a from OscarAnnotation a where a.demographicNo=? and a.tableName=? and a.tableId=?");
-    	query.setParameter(1, demoNo);
-    	query.setParameter(2, tableName);
-    	query.setParameter(3, tableId);
-        @SuppressWarnings("unchecked")
-        List<OscarAnnotation> codeList = query.getResultList();
-        if(codeList.size()>0) {
-        	return codeList.get(0);
-        }
-        return null;
-    }
-
-
-    public void save(OscarAnnotation anno) {
-        if (anno.isUuidSet()){
-            UUID uuid = UUID.randomUUID();
-            anno.setUuid(uuid.toString());
-        }
-        persist(anno);
-    }
-
-
-
-   public int getNumberOfNotes(String demoNo, String tableName,Long tableId){
-   	Query query = entityManager.createQuery("select a from OscarAnnotation a where a.demographicNo=? and a.tableName=? and a.tableId=?");
-   	query.setParameter(1, demoNo);
-   	query.setParameter(2, tableName);
-   	query.setParameter(3,tableId);
-
-   	@SuppressWarnings("unchecked")
-    List<OscarAnnotation> codeList = query.getResultList();
-
-   	return codeList.size();
-   }
+public interface OscarAnnotationDao extends AbstractDao<OscarAnnotation> {
+    OscarAnnotation getAnnotations(String demoNo, String tableName, Long tableId);
+    void save(OscarAnnotation anno);
+    int getNumberOfNotes(String demoNo, String tableName, Long tableId);
 }
