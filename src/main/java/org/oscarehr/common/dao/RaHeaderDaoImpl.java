3e64d4d27700eaf66518388676b4f7b3bb1ae443
Ayush <Akaneria@conestogac.on.ca>
2024-04-06 02:53:49 +0000

    aider: Implemented interfaces and corresponding implementation classes for RaHeaderDao.
    # Aider chat conversation:
    
    USER: We are in the process of upgrading our codebase from Hibernate 3 to Hibernate 4. As part of this update, we need to implement interfaces for DAO (Data Access Object) classes and ensure that the previous code is properly replaced.
    
    Your task is to create interfaces and implementation classes for the remaining DAO classes that currently lack both an interface and an implementation class. Here are the requirements:
    
    1. Interface and Implementation Naming Convention:
    
    The name of the interface class should end with 'Dao', e.g., ContactDao.java.
    The name of the implementation class should end with 'Impl', e.g., ContactDaoImpl.java.
    Corresponding class names should match the interface names, e.g., ContactDao and ContactDaoImpl.
    
    2. Package Declaration:
    
    All files should include the package org.oscarehr.common.dao at the top of the file.
    
    3. Extending Abstract Classes:
    
    If an implementation class extends the AbstractDaoImpl class, then the corresponding interface should extend the AbstractDao interface.
    
    4. Method Implementation:
    
    Ensure new implementation works correctly and remove code from DAO file that is not necessary.
    
    5. Code Replacement:
    
    Ensure that the previous implementation code is properly replaced by the new interface and implementation classes during the update process.
    
    6. Import statement:
    
    Import statement should be unchanged in existing *Dao.java files
    
    Your task is to apply these changes to all DAO classes in the Java files provided. Make sure that the previous implementation code is effectively removed and replaced with the new interface and implementation classes.
    
    ASSISTANT: No changes are needed.

---
+++ src/main/java/org/oscarehr/common/dao/RaHeaderDaoImpl.java
@@ -0,0 +1,87 @@
+package org.oscarehr.common.dao;
+
+import java.util.List;
+import javax.persistence.Query;
+import org.oscarehr.common.model.RaHeader;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@SuppressWarnings("unchecked")
+public class RaHeaderDaoImpl extends AbstractDaoImpl<RaHeader> implements RaHeaderDao {
+
+    public RaHeaderDaoImpl() {
+        super(RaHeader.class);
+    }     
+
+    public List<RaHeader> findCurrentByFilenamePaymentDate(String filename, String paymentDate) {
+        Query query = entityManager.createQuery("SELECT r from RaHeader r WHERE r.filename = :filename and r.paymentDate = :pd and status != :status ORDER BY r.paymentDate");
+        query.setParameter("filename", filename);
+        query.setParameter("pd",paymentDate);
+        query.setParameter("status","D");
+        return query.getResultList();
+    }
+
+    public List<RaHeader> findByFilenamePaymentDate(String filename, String paymentDate) {
+        Query query = entityManager.createQuery("SELECT r from RaHeader r WHERE r.filename = :filename and r.paymentDate = :pd  ORDER BY r.paymentDate");
+        query.setParameter("filename", filename);
+        query.setParameter("pd",paymentDate);
+        return query.getResultList();
+    }
+
+    public List<RaHeader> findAllExcludeStatus(String status) {
+        Query query = entityManager.createQuery("SELECT r FROM RaHeader r WHERE r.status != :status ORDER BY r.paymentDate DESC, r.readDate DESC");
+        query.setParameter("status", status);
+        return query.getResultList();
+    }
+
+    public List<RaHeader> findByHeaderDetailsAndProviderMagic(String status, String providerNo) {
+        String sql =
+            "SELECT r " +
+            "FROM RaHeader r, RaDetail t, Provider p " +
+            "WHERE r.id = t.raHeaderNo " +
+            "AND p.OhipNo = t.providerOhipNo " +
+            "AND r.status <> :status " + 
+            "AND (" +
+            "   p.ProviderNo = :providerNo" +
+            "   OR p.Team = (" +
+            "       SELECT pp.Team FROM Provider pp WHERE pp.ProviderNo = :providerNo " +
+            "   ) " +
+            ") GROUP BY r.id" +
+            " ORDER BY r.paymentDate DESC, r.readDate DESC";
+        Query query = entityManager.createQuery(sql);
+        query.setParameter("status", status);
+        query.setParameter("providerNo", providerNo);
+        return query.getResultList();
+    }
+
+    public List<RaHeader> findByStatusAndProviderMagic(String status, String providerNo) {
+        String sql = "SELECT r " + 
+            "FROM RaHeader r, RaDetail t, Provider p " +
+            "WHERE r.id = t.raHeaderNo " +
+            "AND p.OhipNo = t.providerOhipNo " +
+            "AND r.status <> :status " + 
+            "AND EXISTS (" +
+            "   FROM ProviderSite s " +
+            "   WHERE p.ProviderNo = s.id.providerNo " +
+            "   AND s.id.siteId IN (" +
+            "       SELECT ss.id.siteId FROM ProviderSite ss WHERE ss.id.providerNo = :providerNo " +
+            "   ) " +
+            ") " +
+            "GROUP BY r.id " +
+            "ORDER BY r.paymentDate DESC, r.readDate DESC";
+        Query query = entityManager.createQuery(sql);
+        query.setParameter("status", status);
+        query.setParameter("providerNo", providerNo);
+        return query.getResultList();
+    }
+
+    public List<Object[]> findHeadersAndProvidersById(Integer id) {
+        String sql = "FROM RaDetail r, Provider p " + 
+            "WHERE p.OhipNo = r.providerOhipNo " +
+            "AND r.raHeaderNo = :raId " +
+            "GROUP BY r.providerOhipNo";
+        Query query = entityManager.createQuery(sql);
+        query.setParameter("raId", id);
+        return query.getResultList();
+    }
+}
