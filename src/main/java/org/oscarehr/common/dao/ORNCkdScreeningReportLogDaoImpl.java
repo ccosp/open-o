b9226876437e1e0b3577f893c55c8f703e016b0f
Ayush <Akaneria@conestogac.on.ca>
2024-04-06 02:41:40 +0000

    aider: Implemented interface and implementation classes for ORNCkdScreeningReportLogDao.
    # Aider chat conversation:
    
    USER: We are in the process of upgrading our codebase from Hibernate 3 to Hibernate 4. As part of this update, we need to implement interfaces for DAO (Data Access Object) classes and ensure that the previous code is properly replaced.
    
    Your task is to create interfaces and implementation classes for the remaining DAO classes that currently lack both an interface and an implementation class. Here are the requirements:
    
    1. Interface and Implementation Naming Convention:
    
    The name of the interface class should end with 'Dao', e.g., ContactDao.java.
    The name of the implementation class should end with 'Impl', e.g., ContactDaoImpl.java.
    Corresponding class names should match the interface names, e.g., ContactDao and ContactDaoImpl.
    
    2. Package Declaration:
    
    All files should include the package org.oscarehr.common.dao at the top of the file.
    
    3. Extending Abstract Classes:
    
    If an implementation class extends the AbstractDaoImpl class, then the corresponding interface should extend the AbstractDao interface.
    
    4. Method Implementation:
    
    Ensure new implementation works correctly and remove code from DAO file that is not necessary.
    
    5. Code Replacement:
    
    Ensure that the previous implementation code is properly replaced by the new interface and implementation classes during the update process.
    
    6. Import statement:
    
    Import statement should be unchanged in existing *Dao.java files
    
    Your task is to apply these changes to all DAO classes in the Java files provided. Make sure that the previous implementation code is effectively removed and replaced with the new interface and implementation classes.
    
    ASSISTANT: No changes are needed.

--- src/main/java/org/oscarehr/common/dao/ORNCkdScreeningReportLogDaoImpl.java
+++ src/main/java/org/oscarehr/common/dao/ORNCkdScreeningReportLogDaoImpl.java
@@ -24,8 +24,22 @@
 package org.oscarehr.common.dao;
 
 import java.util.List;
+import javax.persistence.Query;
 import org.oscarehr.common.model.ORNCkdScreeningReportLog;
+import org.springframework.stereotype.Repository;
 
-public interface ORNCkdScreeningReportLogDao extends AbstractDao<ORNCkdScreeningReportLog> {
-	List<ORNCkdScreeningReportLog> getAllReports();
+@Repository
+public class ORNCkdScreeningReportLogDaoImpl extends AbstractDaoImpl<ORNCkdScreeningReportLog> implements ORNCkdScreeningReportLogDao {
+
+	public ORNCkdScreeningReportLogDaoImpl() {
+		super(ORNCkdScreeningReportLog.class);
+	}
+	
+	@Override
+	public List<ORNCkdScreeningReportLog> getAllReports() {
+		Query query = entityManager.createQuery("SELECT x FROM ORNCkdScreeningReportLog x ORDER BY x.lastUpdateDate DESC");
+		@SuppressWarnings("unchecked")
+        List<ORNCkdScreeningReportLog> results = query.getResultList();
+		return results;
+	}
 }
