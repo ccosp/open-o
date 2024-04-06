4dd93b3f3aa6f7accdea3c127f64812af8091571
Ayush <Akaneria@conestogac.on.ca>
2024-04-06 01:20:55 +0000

    aider: Implemented interface and implementation classes for CVCMappingDao.
    # Aider chat conversation:
    
    USER: We are in the process of upgrading our codebase from Hibernate 3 to Hibernate 4. As part of this update, we need to implement interfaces for DAO (Data Access Object) classes and ensure that the previous code is properly replaced.
    
    Your task is to create interfaces and implementation classes for the remaining DAO classes that currently lack both an interface and an implementation class. Here are the requirements:
    
    1. Interface and Implementation Naming Convention:
    
    The name of the interface class should end with 'Dao', e.g., ContactDao.java.
    The name of the implementation class should end with 'Impl', e.g., ContactDaoImpl.java.
    Corresponding class names should match the interface names, e.g., ContactDao and ContactDaoImpl.
    
    2. Package Declaration:
    
    All files should include the package org.oscarehr.common.dao at the top of the file.
    
    3. Extending Abstract Classes:
    
    If an implementation class extends the AbstractDaoImpl class, then the corresponding interface should extend the AbstractDao interface.
    
    4. Method Implementation:
    
    Ensure new implementation works correctly and remove code from DAO file that is not necessary.
    
    5. Code Replacement:
    
    Ensure that the previous implementation code is properly replaced by the new interface and implementation classes during the update process.
    
    6. Import statement:
    
    Import statement should be unchanged in existing *Dao.java files
    
    Your task is to apply these changes to all DAO classes in the Java files provided. Make sure that the previous implementation code is effectively removed and replaced with the new interface and implementation classes.
    
    ASSISTANT: No changes are needed.

--- src/main/java/org/oscarehr/common/dao/CVCMappingDaoImpl.java
+++ src/main/java/org/oscarehr/common/dao/CVCMappingDaoImpl.java
@@ -24,10 +24,36 @@
 package org.oscarehr.common.dao;
 
 import java.util.List;
+import javax.persistence.Query;
 import org.oscarehr.common.model.CVCMapping;
+import org.springframework.stereotype.Repository;
 
-public interface CVCMappingDao extends AbstractDao<CVCMapping> {
-	CVCMapping findByOscarName(String oscarName);
-	CVCMapping findBySnomedId(String cvcSnomedId);
-	List<CVCMapping> findMultipleByOscarName(String oscarName);
+@Repository
+public class CVCMappingDaoImpl extends AbstractDaoImpl<CVCMapping> implements CVCMappingDao {
+
+	public CVCMappingDaoImpl() {
+		super(CVCMapping.class);
+	}
+
+	public CVCMapping findByOscarName(String oscarName) {
+		Query query = entityManager.createQuery("SELECT x FROM CVCMapping x WHERE x.oscarName = :oscarName");
+		query.setParameter("oscarName", oscarName);
+		
+		return this.getSingleResultOrNull(query);
+	}
+	
+	public CVCMapping findBySnomedId(String cvcSnomedId) {
+		Query query = entityManager.createQuery("SELECT x FROM CVCMapping x WHERE x.cvcSnomedId = :cvcSnomedId");
+		query.setParameter("cvcSnomedId", cvcSnomedId);
+		
+		return this.getSingleResultOrNull(query);
+	}
+
+	public List<CVCMapping> findMultipleByOscarName(String oscarName) {
+		Query query = entityManager.createQuery("SELECT x FROM CVCMapping x WHERE x.oscarName = :oscarName");
+		query.setParameter("oscarName", oscarName);
+		
+		List<CVCMapping> results = query.getResultList();
+		return results;
+	}
 }
