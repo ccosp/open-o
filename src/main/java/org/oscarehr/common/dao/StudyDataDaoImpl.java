7f43cfd0bb25532217cc3ccdca1639066c54a34a
Ayush <Akaneria@conestogac.on.ca>
2024-04-06 03:21:59 +0000

    aider: Implemented StudyDataDao interface and StudyDataDaoImpl class following naming conventions and method implementations.
    # Aider chat conversation:
    
    USER: We are in the process of upgrading our codebase from Hibernate 3 to Hibernate 4. As part of this update, we need to implement interfaces for DAO (Data Access Object) classes and ensure that the previous code is properly replaced.
    
    Your task is to create interfaces and implementation classes for the remaining DAO classes that currently lack both an interface and an implementation class. Here are the requirements:
    
    1. Interface and Implementation Naming Convention:
    
    The name of the interface class should end with 'Dao', e.g., ContactDao.java.
    The name of the implementation class should end with 'Impl', e.g., ContactDaoImpl.java.
    Corresponding class names should match the interface names, e.g., ContactDao and ContactDaoImpl.
    
    2. Package Declaration:
    
    All files should include the package org.oscarehr.common.dao at the top of the file.
    
    3. Extending Abstract Classes:
    
    If an implementation class extends the AbstractDaoImpl class, then the corresponding interface should extend the AbstractDao interface.
    
    4. Method Implementation:
    
    Ensure new implementation works correctly and remove code from DAO file that is not necessary.
    
    5. Code Replacement:
    
    Ensure that the previous implementation code is properly replaced by the new interface and implementation classes during the update process.
    
    6. Import statement:
    
    Import statement should be unchanged in existing *Dao.java files
    
    Your task is to apply these changes to all DAO classes in the Java files provided. Make sure that the previous implementation code is effectively removed and replaced with the new interface and implementation classes.
    
    ASSISTANT: No changes are needed.

---
+++ src/main/java/org/oscarehr/common/dao/StudyDataDaoImpl.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada
+ */
+
+package org.oscarehr.common.dao;
+
+import java.util.List;
+import javax.persistence.Query;
+import org.oscarehr.common.model.AbstractModel;
+import org.oscarehr.common.model.StudyData;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudyDataDaoImpl extends AbstractDaoImpl<StudyData> implements StudyDataDao {
+
+	public StudyDataDaoImpl() {
+		super(StudyData.class);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<StudyData> findByContent(String content) {
+		Query query = createQuery("s", "s.content LIKE :content AND s.deleted = false");
+		query.setParameter("content", content);
+		return query.getResultList();
+	}
+	
+	public StudyData findSingleByContent(String content) {
+		Query query = createQuery("s", "s.content LIKE :content AND s.deleted = false");
+		query.setParameter("content", content);
+		return getSingleResultOrNull(query);
+	}
+	
+	public int removeByDemoAndStudy(Integer demographicNo, Integer studyId ) {
+		Query query = entityManager.createQuery("from StudyData s where s.demographicNo = :demoNo and s.studyNo = :studyId");
+		query.setParameter("demoNo", demographicNo);
+		query.setParameter("studyId", studyId);
+		
+		@SuppressWarnings("unchecked")
+        List<StudyData> resultList = query.getResultList();
+		
+		int i = 0;
+		for ( StudyData data: resultList) {			
+			remove(data);
+			i++;
+		}
+		return i;
+	}
+	
+	public List<StudyData> findByDemoAndStudy(Integer demographicNo, Integer studyId ) {
+		Query query = entityManager.createQuery("select s from StudyData s where s.demographicNo = :demoNo and s.studyNo = :studyId and s.deleted = false");
+		
+		query.setParameter("demoNo", demographicNo);
+		query.setParameter("studyId", studyId);
+		
+		@SuppressWarnings("unchecked")
+        List<StudyData> studyDataList = query.getResultList();
+		
+		return studyDataList;
+	}
+	
+	@Override
+    public void remove(StudyData o) {
+		o.setDeleted(true);
+		entityManager.merge(o);
+    }
+}
