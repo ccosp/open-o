98592202090cec2cb9dfe092b66e44bcd2116990
Ayush <Akaneria@conestogac.on.ca>
2024-04-06 02:00:22 +0000

    aider: Implemented interface and implementation class for HnrDataValidationDao.
    # Aider chat conversation:
    
    USER: We are in the process of upgrading our codebase from Hibernate 3 to Hibernate 4. As part of this update, we need to implement interfaces for DAO (Data Access Object) classes and ensure that the previous code is properly replaced.
    
    Your task is to create interfaces and implementation classes for the remaining DAO classes that currently lack both an interface and an implementation class. Here are the requirements:
    
    1. Interface and Implementation Naming Convention:
    
    The name of the interface class should end with 'Dao', e.g., ContactDao.java.
    The name of the implementation class should end with 'Impl', e.g., ContactDaoImpl.java.
    Corresponding class names should match the interface names, e.g., ContactDao and ContactDaoImpl.
    
    2. Package Declaration:
    
    All files should include the package org.oscarehr.common.dao at the top of the file.
    
    3. Extending Abstract Classes:
    
    If an implementation class extends the AbstractDaoImpl class, then the corresponding interface should extend the AbstractDao interface.
    
    4. Method Implementation:
    
    Ensure new implementation works correctly and remove code from DAO file that is not necessary.
    
    5. Code Replacement:
    
    Ensure that the previous implementation code is properly replaced by the new interface and implementation classes during the update process.
    
    6. Import statement:
    
    Import statement should be unchanged in existing *Dao.java files
    
    Your task is to apply these changes to all DAO classes in the Java files provided. Make sure that the previous implementation code is effectively removed and replaced with the new interface and implementation classes.
    
    ASSISTANT: No changes are needed.

--- src/main/java/org/oscarehr/common/dao/HnrDataValidationDaoImpl.java
+++ src/main/java/org/oscarehr/common/dao/HnrDataValidationDaoImpl.java
@@ -23,8 +23,30 @@
 
 package org.oscarehr.common.dao;
 
-import org.oscarehr.common.model.HnrDataValidation;
+import javax.persistence.Query;
 
-public interface HnrDataValidationDao extends AbstractDao<HnrDataValidation> {
-    HnrDataValidation findMostCurrentByFacilityIdClientIdType(Integer facilityId, Integer clientId, HnrDataValidation.Type type);
+import org.oscarehr.common.model.HnrDataValidation;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class HnrDataValidationDaoImpl extends AbstractDaoImpl<HnrDataValidation> implements HnrDataValidationDao {
+
+	public HnrDataValidationDaoImpl() {
+		super(HnrDataValidation.class);
+	}
+
+	@Override
+	public HnrDataValidation findMostCurrentByFacilityIdClientIdType(Integer facilityId, Integer clientId, HnrDataValidation.Type type) {
+		// build sql string
+		String sqlCommand="select * from HnrDataValidation where facilityId=?1 and clientId=?2 and validationType=?3 order by created desc";
+
+		// set parameters
+		Query query = entityManager.createNativeQuery(sqlCommand, modelClass);
+		query.setParameter(1, facilityId);
+		query.setParameter(2, clientId);
+		query.setParameter(3, type.name());
+		
+		// run query
+		return (getSingleResultOrNull(query));
+	}
 }
