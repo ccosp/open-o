728f8920cd60e462712f3da2a1f60a9582c8392d
Ayush <Akaneria@conestogac.on.ca>
2024-04-06 01:38:40 +0000

    aider: Created DrugReasonDao interface and DrugReasonDaoImpl implementation class, following naming conventions and extending AbstractDao and AbstractDaoImpl classes.
    # Aider chat conversation:
    
    USER: We are in the process of upgrading our codebase from Hibernate 3 to Hibernate 4. As part of this update, we need to implement interfaces for DAO (Data Access Object) classes and ensure that the previous code is properly replaced.
    
    Your task is to create interfaces and implementation classes for the remaining DAO classes that currently lack both an interface and an implementation class. Here are the requirements:
    
    1. Interface and Implementation Naming Convention:
    
    The name of the interface class should end with 'Dao', e.g., ContactDao.java.
    The name of the implementation class should end with 'Impl', e.g., ContactDaoImpl.java.
    Corresponding class names should match the interface names, e.g., ContactDao and ContactDaoImpl.
    
    2. Package Declaration:
    
    All files should include the package org.oscarehr.common.dao at the top of the file.
    
    3. Extending Abstract Classes:
    
    If an implementation class extends the AbstractDaoImpl class, then the corresponding interface should extend the AbstractDao interface.
    
    4. Method Implementation:
    
    Ensure new implementation works correctly and remove code from DAO file that is not necessary.
    
    5. Code Replacement:
    
    Ensure that the previous implementation code is properly replaced by the new interface and implementation classes during the update process.
    
    6. Import statement:
    
    Import statement should be unchanged in existing *Dao.java files
    
    Your task is to apply these changes to all DAO classes in the Java files provided. Make sure that the previous implementation code is effectively removed and replaced with the new interface and implementation classes.
    
    ASSISTANT: No changes are needed.

---
+++ src/main/java/org/oscarehr/common/dao/DrugReasonDaoImpl.java
@@ -0,0 +1,102 @@
+/**
+ *
+ * Copyright (c) 2005-2012. Centre for Research on Inner City Health, St. Michael's Hospital, Toronto. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This software was written for
+ * Centre for Research on Inner City Health, St. Michael's Hospital,
+ * Toronto, Ontario, Canada
+ */
+
+package org.oscarehr.common.dao;
+
+import java.util.List;
+import javax.persistence.Query;
+import org.oscarehr.common.model.DrugReason;
+import org.oscarehr.util.MiscUtils;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DrugReasonDaoImpl extends AbstractDaoImpl<DrugReason> implements DrugReasonDao {
+
+	public DrugReasonDaoImpl() {
+		super(DrugReason.class);
+	}
+
+    public boolean addNewDrugReason(DrugReason d){
+       MiscUtils.getLogger().debug("trying to save a drug reason" );
+        		
+       try{
+            entityManager.persist(d);
+        }catch(Exception e){
+            MiscUtils.getLogger().error("Error", e);
+            return false;
+        }
+       return true;
+    }
+    
+    
+    public Boolean hasReason(Integer drugId,String codingSystem,String code, boolean onlyActive){
+    	boolean hasReason = false;
+    	String sqlCommand = "select x from DrugReason x where x.drugId=?1 and x.codingSystem = ?2 and x.code = ?3 and x.archivedFlag = ?4";
+
+        Query query = entityManager.createQuery(sqlCommand);
+        query.setParameter(1, drugId);
+        query.setParameter(2, codingSystem);
+        query.setParameter(3, code);
+        query.setParameter(4, !onlyActive);
+        
+
+        @SuppressWarnings("unchecked")
+        List<DrugReason> results = query.getResultList();
+        if(results != null && results.size() > 0){
+        	hasReason = true;
+        }
+        return hasReason;
+    }
+    
+    
+    public List<DrugReason>  getReasonsForDrugID(Integer drugId,boolean onlyActive){
+    	
+    	String sqlCommand = "select x from DrugReason x where x.drugId=?1 and x.archivedFlag = ?2";
+		
+        Query query = entityManager.createQuery(sqlCommand);
+        query.setParameter(1, drugId);
+        query.setParameter(2,!onlyActive);
+	
+        @SuppressWarnings("unchecked")
+        List<DrugReason> results = query.getResultList();
+
+        return (results);
+    }
+
+    
+    public List<DrugReason> getReasonsByIcd9CodeAndDemographicNo(String icd9Code, Integer demographicNo) {
+    	String sqlCommand = "select x from DrugReason x where x.codingSystem=?1 and x.code= ?2 and x.demographicNo = ?3 and x.archivedFlag = ?4";
+		
+        Query query = entityManager.createQuery(sqlCommand);
+        query.setParameter(1, "icd9");
+        query.setParameter(2,icd9Code);
+        query.setParameter(3, demographicNo);
+        query.setParameter(4,false);
+	
+        @SuppressWarnings("unchecked")
+        List<DrugReason> results = query.getResultList();
+
+        return (results);
+    }
+
+}
