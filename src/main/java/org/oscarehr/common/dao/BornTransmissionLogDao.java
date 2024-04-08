3fe484e9d43d55d7e930876627b4f81f0938e82c
Ayush <Akaneria@conestogac.on.ca>
2024-04-08 20:44:18 +0000

    Added missing import statements

--- src/main/java/org/oscarehr/common/dao/BornTransmissionLogDao.java
+++ src/main/java/org/oscarehr/common/dao/BornTransmissionLogDao.java
@@ -1,6 +1,7 @@
 package org.oscarehr.common.dao;
 
 import java.util.Date;
+import org.oscarehr.common.model.BornTransmissionLog;
 
 public interface BornTransmissionLogDao extends AbstractDao<BornTransmissionLog>  {
     Long getSeqNoToday(String filenameStart, Integer id);
