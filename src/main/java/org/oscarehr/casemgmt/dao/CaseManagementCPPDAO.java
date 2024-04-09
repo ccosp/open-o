40f3e640891f88cc9918f967e47741cd0a017e5f
AparnaBadugu12 <Abadugu@conestogac.on.ca>
2024-04-09 20:18:10 +0000

    Fix for adding interfaces in casemanagement module

--- src/main/java/org/oscarehr/casemgmt/dao/CaseManagementCPPDAO.java
+++ src/main/java/org/oscarehr/casemgmt/dao/CaseManagementCPPDAO.java
@@ -34,41 +34,9 @@ import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
 /*
  * Updated by Eugene Petruhin on 09 jan 2009 while fixing #2482832 & #2494061
  */
-public class CaseManagementCPPDAO extends HibernateDaoSupport {
+public interface CaseManagementCPPDAO {
 
-    private Logger log=MiscUtils.getLogger();
-    
-    public CaseManagementCPP getCPP(String demographic_no) {
-        List results = this.getHibernateTemplate().find("from CaseManagementCPP cpp where cpp.demographic_no = ? order by update_date desc", new Object[] {demographic_no});
-        return (results.size() != 0)?(CaseManagementCPP)results.get(0):null;
-    }
+    public CaseManagementCPP getCPP(String demographic_no);
 
-    public void saveCPP(CaseManagementCPP cpp) {                        
-        
-        String fhist = cpp.getFamilyHistory() == null?"":cpp.getFamilyHistory();
-        String mhist = cpp.getMedicalHistory() == null?"":cpp.getMedicalHistory();
-        String ongoing = cpp.getOngoingConcerns() == null?"":cpp.getOngoingConcerns();
-        String rem = cpp.getReminders() == null?"":cpp.getReminders();
-        String shist = cpp.getSocialHistory() == null?"":cpp.getSocialHistory();
-        String ofnum = cpp.getOtherFileNumber() == null?"":cpp.getOtherFileNumber();
-        String ossystem = cpp.getOtherSupportSystems() == null?"":cpp.getOtherSupportSystems();
-        String pm = cpp.getPastMedications() == null?"":cpp.getPastMedications();
-        
-        cpp.setFamilyHistory(fhist);
-        cpp.setMedicalHistory(mhist);
-        cpp.setOngoingConcerns(ongoing);
-        cpp.setReminders(rem);
-        cpp.setSocialHistory(shist);
-        cpp.setUpdate_date(new Date());
-        cpp.setOtherFileNumber(ofnum);
-        cpp.setOtherSupportSystems(ossystem);
-        cpp.setPastMedications(pm);         
-
-        if (log.isDebugEnabled()) {
-            log.debug("Saving or updating a CPP: " + cpp);
-        }
-
-        this.getHibernateTemplate().saveOrUpdate(cpp);
-        
-    }
+    public void saveCPP(CaseManagementCPP cpp);
 }
