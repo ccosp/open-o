9de7b22f1733b026a079d7ce0ab018f32412dc0e
Ayush <Akaneria@conestogac.on.ca>
2024-04-10 17:55:08 +0000

    aider: Created interface and implementation for LookupManager.
    # Aider chat conversation:
    
    USER: I am updating my code from Hibernate 3 to Hibernate 4. To do this, we need to implement interfaces for Manager classes. I have already implemented this in some files, like src/main/java/com/quatro/service/security/RolesManager.java. Now, I need you to create interfaces and implementation classes for the remaining Manager classes that currently lack both an interface and an implementation class. Write All the code without any placeholder comments. Here are the guidelines for making these changes: 1. Ensure that the name of the implementation class ends with 'Impl' and the interface name ends with 'Manager'. For example, an interface class should be named RolesManager.java and its implementation should be RolesManagerImpl.java. 2. All interfaces and implementation classes should have the same package at the top of the file. 3. Write the full code for classes and methods, and do not include any placeholder comments in place of existing methods 4.Make the interface and Implementation for the com/quatro/service/LookupManager.java
    
    ASSISTANT: No changes are needed.

---
+++ src/main/java/com/quatro/service/impl/LookupManagerImpl.java
@@ -0,0 +1,56 @@
+package com.quatro.service.impl;
+
+import java.sql.SQLException;
+import java.util.List;
+import com.quatro.dao.LookupDao;
+import com.quatro.model.LookupCodeValue;
+import com.quatro.model.LookupTableDefValue;
+import com.quatro.service.LookupManager;
+
+public class LookupManagerImpl implements LookupManager {
+    private LookupDao lookupDao=null;
+
+    public List LoadCodeList(String tableId, boolean activeOnly, String code, String codeDesc) {
+        return lookupDao.LoadCodeList(tableId, activeOnly, code, codeDesc);
+    }
+
+    public List LoadCodeList(String tableId, boolean activeOnly, String parentCode, String code, String codeDesc) {
+        return lookupDao.LoadCodeList(tableId, activeOnly,parentCode, code, codeDesc);
+    }
+
+    public LookupTableDefValue GetLookupTableDef(String tableId){
+        return lookupDao.GetLookupTableDef(tableId);
+    }
+
+    public LookupCodeValue GetLookupCode(String tableId, String code){
+        return lookupDao.GetCode(tableId, code);
+    }
+
+    public List LoadFieldDefList(String tableId) {
+        return lookupDao.LoadFieldDefList(tableId);
+    }
+
+    public List GetCodeFieldValues(LookupTableDefValue tableDef, String code) {
+        return lookupDao.GetCodeFieldValues(tableDef, code);
+    }
+
+    public List GetCodeFieldValues(LookupTableDefValue tableDef) {
+        return lookupDao.GetCodeFieldValues(tableDef);
+    }
+
+    public String SaveCodeValue(boolean isNew, LookupTableDefValue tableDef, List fieldDefList) throws SQLException {
+        return lookupDao.SaveCodeValue(isNew, tableDef, fieldDefList);
+    }
+
+    public int getCountOfActiveClient(String orgCd) throws SQLException{
+        return lookupDao.getCountOfActiveClient(orgCd); 
+    }
+
+    public LookupDao getLookupDao() {
+        return lookupDao;
+    }
+
+    public void setLookupDao(LookupDao lookupDao) {
+        this.lookupDao = lookupDao;
+    }
+}
