f885036b02c9adb6dcef04aa077ca1ec7ea1f8b7
Ayush <Akaneria@conestogac.on.ca>
2024-04-10 17:55:08 +0000

    aider: Created interface and implementation for LookupManager.
    # Aider chat conversation:
    
    USER: I am updating my code from Hibernate 3 to Hibernate 4. To do this, we need to implement interfaces for Manager classes. I have already implemented this in some files, like src/main/java/com/quatro/service/security/RolesManager.java. Now, I need you to create interfaces and implementation classes for the remaining Manager classes that currently lack both an interface and an implementation class. Write All the code without any placeholder comments. Here are the guidelines for making these changes: 1. Ensure that the name of the implementation class ends with 'Impl' and the interface name ends with 'Manager'. For example, an interface class should be named RolesManager.java and its implementation should be RolesManagerImpl.java. 2. All interfaces and implementation classes should have the same package at the top of the file. 3. Write the full code for classes and methods, and do not include any placeholder comments in place of existing methods 4.Make the interface and Implementation for the com/quatro/service/LookupManager.java
    
    ASSISTANT: No changes are needed.

--- src/main/java/com/quatro/service/LookupManager.java
+++ src/main/java/com/quatro/service/LookupManager.java
@@ -1,78 +1,18 @@
-/**
- * Copyright (c) 2005, 2009 IBM Corporation and others.
- * This software is published under the GPL GNU General Public License.
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *
- * Contributors:
- *     <Quatro Group Software Systems inc.>  <OSCAR Team>
- */
 package com.quatro.service;
 
 import java.sql.SQLException;
 import java.util.List;
-
-import com.quatro.dao.LookupDao;
 import com.quatro.model.LookupCodeValue;
 import com.quatro.model.LookupTableDefValue;
 
-public class LookupManager {
-    private LookupDao lookupDao=null;
-
-	public List LoadCodeList(String tableId, boolean activeOnly, String code, String codeDesc) {
-        return lookupDao.LoadCodeList(tableId, activeOnly, code, codeDesc);
-	}
-
-	public List LoadCodeList(String tableId, boolean activeOnly, String parentCode, String code, String codeDesc) {
-        return lookupDao.LoadCodeList(tableId, activeOnly,parentCode, code, codeDesc);
-	}
-
-    public LookupTableDefValue GetLookupTableDef(String tableId){
-        return lookupDao.GetLookupTableDef(tableId);
-    }
-	
-    public LookupCodeValue GetLookupCode(String tableId, String code){
-        return lookupDao.GetCode(tableId, code);
-    }
-
-    public LookupDao getLookupDao() {
-		return lookupDao;
-	}
-
-	public void setLookupDao(LookupDao lookupDao) {
-		this.lookupDao = lookupDao;
-	}
-	
-	public List LoadFieldDefList(String tableId)
-	{
-		return lookupDao.LoadFieldDefList(tableId);
-	}
-	public List GetCodeFieldValues(LookupTableDefValue tableDef, String code)
-	{
-		return lookupDao.GetCodeFieldValues(tableDef, code);
-	}
-	public List GetCodeFieldValues(LookupTableDefValue tableDef)
-	{
-		return lookupDao.GetCodeFieldValues(tableDef);
-	}
-	public String SaveCodeValue(boolean isNew, LookupTableDefValue tableDef, List fieldDefList) throws SQLException
-	{
-		return lookupDao.SaveCodeValue(isNew, tableDef, fieldDefList);
-	}
-	
-	public int getCountOfActiveClient(String orgCd) throws SQLException{
-   	    return lookupDao.getCountOfActiveClient(orgCd); 
-	}
-
+public interface LookupManager {
+    List LoadCodeList(String tableId, boolean activeOnly, String code, String codeDesc);
+    List LoadCodeList(String tableId, boolean activeOnly, String parentCode, String code, String codeDesc);
+    LookupTableDefValue GetLookupTableDef(String tableId);
+    LookupCodeValue GetLookupCode(String tableId, String code);
+    List LoadFieldDefList(String tableId);
+    List GetCodeFieldValues(LookupTableDefValue tableDef, String code);
+    List GetCodeFieldValues(LookupTableDefValue tableDef);
+    String SaveCodeValue(boolean isNew, LookupTableDefValue tableDef, List fieldDefList) throws SQLException;
+    int getCountOfActiveClient(String orgCd) throws SQLException;
 }
