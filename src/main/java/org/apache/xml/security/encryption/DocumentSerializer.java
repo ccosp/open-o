5be4696dfd5581caac5a4ff6ecf44cce5aa989a3
Deval Italiya <deval.italiya@magentahealth.ca>
2024-06-06 22:52:46 +0000

    Downgrade xmlsec version from 2.2.3 to 2.1.8

--- src/main/java/org/apache/xml/security/encryption/DocumentSerializer.java
+++ src/main/java/org/apache/xml/security/encryption/DocumentSerializer.java
@@ -4,9 +4,12 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.StringReader;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.commons.codec.binary.Base64;
+import org.apache.xml.security.c14n.InvalidCanonicalizerException;
 import org.apache.xml.security.utils.XMLUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.DocumentFragment;
