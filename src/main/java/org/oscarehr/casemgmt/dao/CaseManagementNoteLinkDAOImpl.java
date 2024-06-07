8eb045635e83522f5dba53ce967ea5f29886316b
Kate Yang <150059882+kateyang1998@users.noreply.github.com>
2024-06-07 15:32:29 -0400

    feat(fix): Change @Transactional for tickler related notes.

--- src/main/java/org/oscarehr/casemgmt/dao/CaseManagementNoteLinkDAOImpl.java
+++ src/main/java/org/oscarehr/casemgmt/dao/CaseManagementNoteLinkDAOImpl.java
@@ -30,7 +30,9 @@ import java.util.List;
 
 import org.oscarehr.casemgmt.model.CaseManagementNoteLink;
 import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public class CaseManagementNoteLinkDAOImpl extends HibernateDaoSupport implements CaseManagementNoteLinkDAO {
 
     @Override
