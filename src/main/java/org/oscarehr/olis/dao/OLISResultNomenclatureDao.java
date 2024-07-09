ec6896e8ff8b390ba75f9e579357a30d3962bac4
Kate Yang <150059882+kateyang1998@users.noreply.github.com>
2024-07-09 12:40:20 -0400

    feat(fix): Fixed the error caused by unknown parameter position when loading echart.

--- src/main/java/org/oscarehr/olis/dao/OLISResultNomenclatureDao.java
+++ src/main/java/org/oscarehr/olis/dao/OLISResultNomenclatureDao.java
@@ -27,7 +27,7 @@ public class OLISResultNomenclatureDao extends AbstractDaoImpl<OLISResultNomencl
 	public OLISResultNomenclature findByNameId(String id) {
 		String sql = "select x from "+ this.modelClass.getName() + " x where x.nameId=?";
 		Query query = entityManager.createQuery(sql);
-		query.setParameter(1, id);		
+		query.setParameter(0, id);		
 		return this.getSingleResultOrNull(query);
 	}
 	
