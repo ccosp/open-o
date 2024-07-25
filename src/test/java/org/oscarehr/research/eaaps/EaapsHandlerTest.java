534beca6f90aa8bcd3f35bf88911ba8359f61486
ayushkaneria <Akaneria@conestogac.on.ca>
2024-07-25 19:49:21 +0000

    Commented EaapsHandlerTest as of now. It will be removed under ASOS-188 Dead code removals of Eaaps.

--- src/test/java/org/oscarehr/research/eaaps/EaapsHandlerTest.java
+++ src/test/java/org/oscarehr/research/eaaps/EaapsHandlerTest.java
@@ -43,100 +43,100 @@ import org.oscarehr.common.model.MessageTbl;
 import org.oscarehr.util.SpringUtils;
 
 import oscar.OscarProperties;
-//Ignored until we fix foreign keys with tests
-@Ignore
-public class EaapsHandlerTest extends DaoTestFixtures {
+// This Test will be removed under ASOS-188 - Dead code removals of Eaaps
+// @Ignore
+// public class EaapsHandlerTest extends DaoTestFixtures {
 	
-	@Before
-	public void beforeTest() {
-		OscarProperties props = OscarProperties.getInstance();
-		props.setProperty("DOCUMENT_DIR", System.getProperty("buildDirectory"));
-	}
+// 	@Before
+// 	public void beforeTest() {
+// 		OscarProperties props = OscarProperties.getInstance();
+// 		props.setProperty("DOCUMENT_DIR", System.getProperty("buildDirectory"));
+// 	}
 	
-	@BeforeClass
-	public static void init() throws Exception {
-		SchemaUtils.restoreAllTables();
-	}
+// 	@BeforeClass
+// 	public static void init() throws Exception {
+// 		SchemaUtils.restoreAllTables();
+// 	}
 	
-	private String getHash(Demographic demo) {
-		String hash = new EaapsHash(demo).getHash();
-		return hash;
-	}
+// 	private String getHash(Demographic demo) {
+// 		String hash = new EaapsHash(demo).getHash();
+// 		return hash;
+// 	}
 	
-	public String getHL7(Demographic demo) {
-		String hash = getHash(demo);
+// 	public String getHL7(Demographic demo) {
+// 		String hash = getHash(demo);
 		
-		String result = 
-				"MSH|^~\\&|SENDING APP||||" + EaapsIntegrationTest.TIMESTAMP_STRING + ".007-0400||ORU^R01|2501|01|2.2|1\r" + 
-						"PID||" + hash + "\r" + 
-						"OBR||||SERVICE ID: EAAPS||||||||||||" + EaapsIntegrationTest.PROVIDER_ID + "\r" + 
-						"NTE|1|eaaps_" + hash + "_" + System.currentTimeMillis() + ".pdf|" + EaapsIntegrationTest.PDF + "\r" +
-						"NTE|2||Note comment for message with the AAP attachment " + hash + "\r" +
-						"NTE|3||MRP message for message with the AAP attachment " + hash + "\r";
-		return result;
-	}
+// 		String result = 
+// 				"MSH|^~\\&|SENDING APP||||" + EaapsIntegrationTest.TIMESTAMP_STRING + ".007-0400||ORU^R01|2501|01|2.2|1\r" + 
+// 						"PID||" + hash + "\r" + 
+// 						"OBR||||SERVICE ID: EAAPS||||||||||||" + EaapsIntegrationTest.PROVIDER_ID + "\r" + 
+// 						"NTE|1|eaaps_" + hash + "_" + System.currentTimeMillis() + ".pdf|" + EaapsIntegrationTest.PDF + "\r" +
+// 						"NTE|2||Note comment for message with the AAP attachment " + hash + "\r" +
+// 						"NTE|3||MRP message for message with the AAP attachment " + hash + "\r";
+// 		return result;
+// 	}
 	
-	@Test
-	public void testProviderParsing() throws Exception {
-		DemographicDao demoDao = SpringUtils.getBean(DemographicDao.class);
-		Demographic demo = demoDao.getDemographic("1");
-		if (demo == null) {
-			demo = new Demographic();
-			demo.setFirstName("John");
-			demo.setLastName("Johnson");
-			demo.setSex("M");
-			demo.setProviderNo("999998");
+// 	@Test
+// 	public void testProviderParsing() throws Exception {
+// 		DemographicDao demoDao = SpringUtils.getBean(DemographicDao.class);
+// 		Demographic demo = demoDao.getDemographic("1");
+// 		if (demo == null) {
+// 			demo = new Demographic();
+// 			demo.setFirstName("John");
+// 			demo.setLastName("Johnson");
+// 			demo.setSex("M");
+// 			demo.setProviderNo("999998");
 			
-			demoDao.save(demo);
-		}
+// 			demoDao.save(demo);
+// 		}
 		
-		DxresearchDAO dxresearchDAO = SpringUtils.getBean(DxresearchDAO.class);
-		Dxresearch dxresearch = new Dxresearch();
+// 		DxresearchDAO dxresearchDAO = SpringUtils.getBean(DxresearchDAO.class);
+// 		Dxresearch dxresearch = new Dxresearch();
 		
-		dxresearch.setCodingSystem("icd9");
-		dxresearch.setDemographicNo(demo.getDemographicNo());
-		dxresearch.setStatus('A');
-		dxresearch.setDxresearchCode("493");
+// 		dxresearch.setCodingSystem("icd9");
+// 		dxresearch.setDemographicNo(demo.getDemographicNo());
+// 		dxresearch.setStatus('A');
+// 		dxresearch.setDxresearchCode("493");
 		
-		dxresearchDAO.persist(dxresearch);
+// 		dxresearchDAO.persist(dxresearch);
 		
-		/* Study is no longer running
-		StudyDao studyDao = SpringUtils.getBean(StudyDao.class);
-		Study study = studyDao.findByName("eAAPS Study");
-		if (study == null) {
-			study = new Study();
-			study.setCurrent1(1);
-			study.setDescription("Test study");
-			study.setProviderNo("999998");
-			study.setRemoteServerUrl("http://eaaps");
-			study.setStudyName("eAAPS Study");
-			study.setStudyLink("http://eaaps");
-			study.setTimestamp(new Date());
-			studyDao.saveEntity(study);
-		}
+// 		/* Study is no longer running
+// 		StudyDao studyDao = SpringUtils.getBean(StudyDao.class);
+// 		Study study = studyDao.findByName("eAAPS Study");
+// 		if (study == null) {
+// 			study = new Study();
+// 			study.setCurrent1(1);
+// 			study.setDescription("Test study");
+// 			study.setProviderNo("999998");
+// 			study.setRemoteServerUrl("http://eaaps");
+// 			study.setStudyName("eAAPS Study");
+// 			study.setStudyLink("http://eaaps");
+// 			study.setTimestamp(new Date());
+// 			studyDao.saveEntity(study);
+// 		}
 		
-		StudyDataDao studyDataDao = SpringUtils.getBean(StudyDataDao.class);
-		List<StudyData> data = studyDataDao.findByDemoAndStudy(demo.getDemographicNo(), study.getId());
-		if (data.isEmpty()) {
-			StudyData studyData = new StudyData();
-			studyData.setDemographicNo(demo.getDemographicNo());
-			studyData.setProviderNo("999998");
-			studyData.setContent(getHash(demo));
-			studyDataDao.saveEntity(studyData);
-		}
-		*/
+// 		StudyDataDao studyDataDao = SpringUtils.getBean(StudyDataDao.class);
+// 		List<StudyData> data = studyDataDao.findByDemoAndStudy(demo.getDemographicNo(), study.getId());
+// 		if (data.isEmpty()) {
+// 			StudyData studyData = new StudyData();
+// 			studyData.setDemographicNo(demo.getDemographicNo());
+// 			studyData.setProviderNo("999998");
+// 			studyData.setContent(getHash(demo));
+// 			studyDataDao.saveEntity(studyData);
+// 		}
+// 		*/
 		
-		String hl7 = getHL7(demo);
-		EaapsHandler handler = new EaapsHandler();
-		handler.init(hl7);
+// 		String hl7 = getHL7(demo);
+// 		EaapsHandler handler = new EaapsHandler();
+// 		handler.init(hl7);
 		
-		MessageTblDao mtDao = SpringUtils.getBean(MessageTblDao.class);
-		List<MessageTbl> messages = mtDao.findAll(0, 100);
-		assertFalse(messages.isEmpty());
+// 		MessageTblDao mtDao = SpringUtils.getBean(MessageTblDao.class);
+// 		List<MessageTbl> messages = mtDao.findAll(0, 100);
+// 		assertFalse(messages.isEmpty());
 		
-		MessageTbl m = messages.get(0);
-		assertEquals("Must be sent by system", "System", m.getSentBy());
-		assertEquals("Must be sent by system", "-1", m.getSentByNo());
-	}
+// 		MessageTbl m = messages.get(0);
+// 		assertEquals("Must be sent by system", "System", m.getSentBy());
+// 		assertEquals("Must be sent by system", "-1", m.getSentByNo());
+// 	}
 	
-}
+// }
