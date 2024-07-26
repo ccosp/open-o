097c2e08674be7a691aa8c8a7146a8355cc369c6
Kate Yang <150059882+kateyang1998@users.noreply.github.com>
2024-07-26 11:05:14 -0400

    Implemented development.sql(oscar db) and development-drugref.sql (drugref2 db), cleaned-up data in oscar db.

--- .devcontainer/db/scripts/populate_db.sh
+++ .devcontainer/db/scripts/populate_db.sh
@@ -8,4 +8,6 @@ echo 'Creating development database...'
 ./createdatabase_on.sh root password oscar
 echo 'Creating test database...'
 ./createdatabase_on.sh root password oscar_test
+echo 'Creating drugref2 database...'
+mysql -u root -ppassword drugref2 < /database/mysql/development-drugref.sql
 cd ../../
