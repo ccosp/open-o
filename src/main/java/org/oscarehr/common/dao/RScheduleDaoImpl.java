86f873cf0a25bf54713a4b89341b1e09cb45b1f9
Michael Yingbull <myingbull@conestogac.on.ca>
2024-10-02 00:39:32 -0400

    refactor: update HQL queries to use numbered positional parameters

--- src/main/java/org/oscarehr/common/dao/RScheduleDaoImpl.java
+++ src/main/java/org/oscarehr/common/dao/RScheduleDaoImpl.java
@@ -44,10 +44,10 @@ public class RScheduleDaoImpl extends AbstractDaoImpl<RSchedule> implements RSch
 
     @Override
     public List<RSchedule> findByProviderAvailableAndDate(String providerNo, String available, Date sdate) {
-        Query query = entityManager.createQuery("select s from RSchedule s where s.providerNo=? and s.available=? and s.sDate=?");
-        query.setParameter(0, providerNo);
-        query.setParameter(1, available);
-        query.setParameter(2, sdate);
+        Query query = entityManager.createQuery("select s from RSchedule s where s.providerNo=?1 and s.available=?2 and s.sDate=?3");
+        query.setParameter(1, providerNo);
+        query.setParameter(2, available);
+        query.setParameter(3, sdate);
 
         List<RSchedule> results = query.getResultList();
         return results;
@@ -55,22 +55,22 @@ public class RScheduleDaoImpl extends AbstractDaoImpl<RSchedule> implements RSch
 
     @Override
     public Long search_rschedule_overlaps(String providerNo, Date d1, Date d2, Date d3, Date d4, Date d5, Date d6, Date d7, Date d8, Date d9, Date d10, Date d11, Date d12, Date d13, Date d14) {
-        Query query = entityManager.createQuery("select count(r.id) from RSchedule r where r.providerNo=? and ((r.sDate <? and r.eDate >=?) or (? < r.sDate and r.sDate < ?) or (? < r.eDate and r.eDate <= ?) or ( ? < r.sDate and r.eDate <= ?) or (r.sDate = ? and r.sDate = ?) or (r.eDate = ? and r.eDate <= ?) or (r.sDate = ? and r.eDate != ?)) and r.status = 'A'");
-        query.setParameter(0, providerNo);
-        query.setParameter(1, d1);
-        query.setParameter(2, d2);
-        query.setParameter(3, d3);
-        query.setParameter(4, d4);
-        query.setParameter(5, d5);
-        query.setParameter(6, d6);
-        query.setParameter(7, d7);
-        query.setParameter(8, d8);
-        query.setParameter(9, d9);
-        query.setParameter(10, d10);
-        query.setParameter(11, d11);
-        query.setParameter(12, d12);
-        query.setParameter(13, d13);
-        query.setParameter(14, d14);
+        Query query = entityManager.createQuery("select count(r.id) from RSchedule r where r.providerNo=?1 and ((r.sDate <?2 and r.eDate >=?3) or (?4 < r.sDate and r.sDate < ?5) or (?6 < r.eDate and r.eDate <= ?7) or ( ?8 < r.sDate and r.eDate <= ?9) or (r.sDate = ?10 and r.sDate = ?11) or (r.eDate = ?12 and r.eDate <= ?13) or (r.sDate = ?14 and r.eDate != ?15)) and r.status = 'A'");
+        query.setParameter(1, providerNo);
+        query.setParameter(2, d1);
+        query.setParameter(3, d2);
+        query.setParameter(4, d3);
+        query.setParameter(5, d4);
+        query.setParameter(6, d5);
+        query.setParameter(7, d6);
+        query.setParameter(8, d7);
+        query.setParameter(9, d8);
+        query.setParameter(10, d9);
+        query.setParameter(11, d10);
+        query.setParameter(12, d11);
+        query.setParameter(13, d12);
+        query.setParameter(14, d13);
+        query.setParameter(15, d14);
 
         Long results = (Long) query.getSingleResult();
         return results;
@@ -78,10 +78,10 @@ public class RScheduleDaoImpl extends AbstractDaoImpl<RSchedule> implements RSch
 
     @Override
     public Long search_rschedule_exists(String providerNo, Date d1, Date d2) {
-        Query query = entityManager.createQuery("select count(r.id) from RSchedule r where r.providerNo=? and r.sDate =? and r.eDate =? and r.status = 'A'");
-        query.setParameter(0, providerNo);
-        query.setParameter(1, d1);
-        query.setParameter(2, d2);
+        Query query = entityManager.createQuery("select count(r.id) from RSchedule r where r.providerNo=?1 and r.sDate =?2 and r.eDate =?3 and r.status = 'A'");
+        query.setParameter(1, providerNo);
+        query.setParameter(2, d1);
+        query.setParameter(3, d2);
 
         Long results = (Long) query.getSingleResult();
         return results;
@@ -89,10 +89,10 @@ public class RScheduleDaoImpl extends AbstractDaoImpl<RSchedule> implements RSch
 
     @Override
     public RSchedule search_rschedule_current(String providerNo, String available, Date sdate) {
-        Query query = entityManager.createQuery("select s from RSchedule s where s.providerNo=? and s.available=? and s.sDate <= ? and s.status='A' order by s.sDate desc");
-        query.setParameter(0, providerNo);
-        query.setParameter(1, available);
-        query.setParameter(2, sdate);
+        Query query = entityManager.createQuery("select s from RSchedule s where s.providerNo=?1 and s.available=?2 and s.sDate <= ?3 and s.status='A' order by s.sDate desc");
+        query.setParameter(1, providerNo);
+        query.setParameter(2, available);
+        query.setParameter(3, sdate);
         query.setMaxResults(1);
 
         RSchedule result = this.getSingleResultOrNull(query);
@@ -101,10 +101,10 @@ public class RScheduleDaoImpl extends AbstractDaoImpl<RSchedule> implements RSch
 
     @Override
     public List<RSchedule> search_rschedule_future(String providerNo, String available, Date sdate) {
-        Query query = entityManager.createQuery("select s from RSchedule s where s.providerNo=? and s.available=? and s.sDate > ? and s.status='A' order by s.sDate");
-        query.setParameter(0, providerNo);
-        query.setParameter(1, available);
-        query.setParameter(2, sdate);
+        Query query = entityManager.createQuery("select s from RSchedule s where s.providerNo=?1 and s.available=?2 and s.sDate > ?3 and s.status='A' order by s.sDate");
+        query.setParameter(1, providerNo);
+        query.setParameter(2, available);
+        query.setParameter(3, sdate);
 
         @SuppressWarnings("unchecked")
         List<RSchedule> results = query.getResultList();
@@ -113,9 +113,9 @@ public class RScheduleDaoImpl extends AbstractDaoImpl<RSchedule> implements RSch
 
     @Override
     public RSchedule search_rschedule_current1(String providerNo, Date sdate) {
-        Query query = entityManager.createQuery("select s from RSchedule s where s.providerNo=? and s.sDate <= ? and s.status='A' order by s.sDate desc");
-        query.setParameter(0, providerNo);
-        query.setParameter(1, sdate);
+        Query query = entityManager.createQuery("select s from RSchedule s where s.providerNo=?1 and s.sDate <= ?2 and s.status='A' order by s.sDate desc");
+        query.setParameter(1, providerNo);
+        query.setParameter(2, sdate);
         query.setMaxResults(1);
 
         RSchedule result = this.getSingleResultOrNull(query);
@@ -124,9 +124,9 @@ public class RScheduleDaoImpl extends AbstractDaoImpl<RSchedule> implements RSch
 
     @Override
     public RSchedule search_rschedule_current2(String providerNo, Date sdate) {
-        Query query = entityManager.createQuery("select s from RSchedule s where s.providerNo=? and s.sDate >= ? and s.status='A' order by s.sDate");
-        query.setParameter(0, providerNo);
-        query.setParameter(1, sdate);
+        Query query = entityManager.createQuery("select s from RSchedule s where s.providerNo=?1 and s.sDate >= ?2 and s.status='A' order by s.sDate");
+        query.setParameter(1, providerNo);
+        query.setParameter(2, sdate);
         query.setMaxResults(1);
 
         RSchedule result = this.getSingleResultOrNull(query);
@@ -135,9 +135,9 @@ public class RScheduleDaoImpl extends AbstractDaoImpl<RSchedule> implements RSch
 
     @Override
     public List<RSchedule> search_rschedule_future1(String providerNo, Date sdate) {
-        Query query = entityManager.createQuery("select s from RSchedule s where s.providerNo=? and s.sDate > ? and s.status='A' order by s.sDate");
-        query.setParameter(0, providerNo);
-        query.setParameter(1, sdate);
+        Query query = entityManager.createQuery("select s from RSchedule s where s.providerNo=?1 and s.sDate > ?2 and s.status='A' order by s.sDate");
+        query.setParameter(1, providerNo);
+        query.setParameter(2, sdate);
 
         List<RSchedule> results = query.getResultList();
         return results;
