441f69d38c8dc447a058452181f9cf88fadc05e4
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2024-10-04 15:36:37 -0400

    chore: add verbose comments to scripts
    Update Bash and PowerShell scripts with verbose comments on script purpose.

--- scripts/hibernate-upgrade.sh
+++ scripts/hibernate-upgrade.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# DESCRIPTION
+# This script has been created to automate the upgrade between Hibernate 5.2.18 and 5.6.15.
+#
+# This script runs aider with Deepseek recursively
+# on all files that end with 'dao.java', 'Dao.java',
+# 'DAO.java', 'daoImpl.java', 'DaoImpl.java', or 'DAOImpl.java'.
+# 
+# Aider is prompted to update all JPA HQL queries to use proper
+# positional parameter arguments (e.g. ?1, ?2, ?3).
+# 
+# Ensure that the DEEPSEEK_API_KEY env variable is set to your Deepseek key.
+# See https://aider.chat/docs/llms/deepseek.html for more info.
+
 # Use the current working directory as the target directory
 target_dir=$(pwd)
 
