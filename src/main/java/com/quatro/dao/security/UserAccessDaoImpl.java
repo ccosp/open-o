6bb16da1a7f43ebeb7e48bb9daf204a836c63638
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2024-10-07 14:14:13 -0400

    fix: update positional parameters in UserAccessDaoImpl
    Update queries to use positional parameters starting from 0, since this class is using the old Hibernate library.

--- src/main/java/com/quatro/dao/security/UserAccessDaoImpl.java
+++ src/main/java/com/quatro/dao/security/UserAccessDaoImpl.java
@@ -35,10 +35,10 @@ public class UserAccessDaoImpl extends HibernateDaoSupport implements UserAccess
         String sSQL = "";
         if (shelterId != null && shelterId.intValue() > 0) {
             String s = "'%S" + shelterId.toString() + ",%'";
-            sSQL = "from UserAccessValue s where s.providerNo= ? " +
+            sSQL = "from UserAccessValue s where s.providerNo= ?0 " +
                     " and s.orgCdcsv like " + s + " order by s.functionCd, s.privilege desc, s.orgCd";
         } else {
-            sSQL = "from UserAccessValue s where s.providerNo= ? " +
+            sSQL = "from UserAccessValue s where s.providerNo= ?0 " +
                     " order by s.functionCd, s.privilege desc, s.orgCd";
         }
         return getHibernateTemplate().find(sSQL, providerNo);
@@ -49,12 +49,12 @@ public class UserAccessDaoImpl extends HibernateDaoSupport implements UserAccess
         String sSQL = "";
         if (shelterId != null && shelterId.intValue() > 0) {
             sSQL = "select distinct o.codecsv from UserAccessValue s, LstOrgcd o " +
-                    "where s.providerNo= ? and s.privilege>='r' and s.orgCd=o.code " +
+                    "where s.providerNo= ?0 and s.privilege>='r' and s.orgCd=o.code " +
                     " and o.codecsv like '%S" + shelterId.toString() + ",%'" +
                     " order by o.codecsv";
             return getHibernateTemplate().find(sSQL, providerNo);
         } else {
-            sSQL = "select distinct o.codecsv from UserAccessValue s, LstOrgcd o where s.providerNo= ? and s.privilege>='r' and s.orgCd=o.code order by o.codecsv";
+            sSQL = "select distinct o.codecsv from UserAccessValue s, LstOrgcd o where s.providerNo= ?0 and s.privilege>='r' and s.orgCd=o.code order by o.codecsv";
             return getHibernateTemplate().find(sSQL, providerNo);
         }
     }
