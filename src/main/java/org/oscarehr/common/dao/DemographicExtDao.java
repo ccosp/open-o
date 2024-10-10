d768923bea872273aff329c51c6109655d04eaa5
garciaerin <garciaerinjade@gmail.com>
2024-10-10 10:57:12 -0400

    Revert "chore: remove unused methods from DemographicExtDao interface"
    This reverts commit e66fed5da4b9dbce71b82e6c4b5bccba3fc011e4.

--- src/main/java/org/oscarehr/common/dao/DemographicExtDao.java
+++ src/main/java/org/oscarehr/common/dao/DemographicExtDao.java
@@ -46,4 +46,41 @@ public interface DemographicExtDao extends AbstractDao<DemographicExt> {
 
     public DemographicExt getDemographicExt(Integer demographicNo, String key);
 
+    public List<DemographicExt> getDemographicExtByKeyAndValue(DemographicExtKey demographicExtKey, String value);
+
+    public List<DemographicExt> getDemographicExtByKeyAndValue(String key, String value);
+
+    public DemographicExt getLatestDemographicExt(Integer demographicNo, String key);
+
+    public void updateDemographicExt(DemographicExt de);
+
+    public void saveDemographicExt(Integer demographicNo, String key, String value);
+
+    public void removeDemographicExt(Integer id);
+
+    public void removeDemographicExt(Integer demographicNo, String key);
+
+    public Map<String, String> getAllValuesForDemo(Integer demo);
+
+    public void addKey(String providerNo, Integer demo, String key, String value);
+
+    public void addKey(String providerNo, Integer demo, String key, String newValue, String oldValue);
+
+    public List<String[]> getListOfValuesForDemo(Integer demo);
+
+    public String getValueForDemoKey(Integer demo, String key);
+
+    public List<Integer> findDemographicIdsByKeyVal(DemographicExtKey demographicExtKey, String val);
+
+    public List<Integer> findDemographicIdsByKeyVal(String key, String val);
+
+    public List<DemographicExt> getMultipleDemographicExtKeyForDemographicNumbersByProviderNumber(
+            final DemographicExtKey demographicExtKey,
+            final Collection<Integer> demographicNumbers,
+            final String midwifeNumber);
+
+    public List<Integer> getDemographicNumbersByDemographicExtKeyAndProviderNumberAndDemographicLastNameRegex(
+            final DemographicExtKey key,
+            final String providerNumber,
+            final String lastNameRegex);
 }
