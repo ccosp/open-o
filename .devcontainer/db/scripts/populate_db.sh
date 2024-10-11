53a67f82cd454af2716039e5ddfd49156bd484f6
Kate Yang <150059882+kateyang1998@users.noreply.github.com>
2024-10-11 11:51:35 -0400

    fix: revised the schema to align to Magenta's current one.

--- .devcontainer/db/scripts/populate_db.sh
+++ .devcontainer/db/scripts/populate_db.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env sh
-
-# Magenta-specific
-
 echo 'Setting up all databases...'
 cd /database/mysql || exit 1
 echo 'Creating development database...'
@@ -9,5 +6,8 @@ echo 'Creating development database...'
 echo 'Creating test database...'
 ./createdatabase_on.sh root password oscar_test
 echo 'Creating drugref2 database...'
+mysql -u root -ppassword -e "CREATE DATABASE IF NOT EXISTS drugref2;"
 mysql -u root -ppassword drugref2 < /database/mysql/development-drugref.sql
+echo 'Loading demo data for development...'
+mysql -u root -ppassword oscar < /scripts/development.sql
 cd ../../
