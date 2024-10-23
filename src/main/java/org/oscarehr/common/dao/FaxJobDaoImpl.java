8a4d3a1bce695b263a4cca179d7c485d2f3d6c12
garciaerin <garciaerinjade@gmail.com>
2024-10-23 14:37:30 -0400

    fix: added a counter variable for the if statements in FaxJobDaoImpl.java
    Updated FaxJobDaoImpl.java
    - added counter a variable for each if statement

--- src/main/java/org/oscarehr/common/dao/FaxJobDaoImpl.java
+++ src/main/java/org/oscarehr/common/dao/FaxJobDaoImpl.java
@@ -56,18 +56,19 @@ public class FaxJobDaoImpl extends AbstractDaoImpl<FaxJob> implements FaxJobDao 
         }
 
         boolean firstclause = false;
+        int counter = 1;
 
         if (demographic_no != null) {
             firstclause = true;
-            sql.append(" job.demographicNo = :demo");
+            sql.append(" job.demographicNo = :demo" + counter++);
         }
 
         if (status != null) {
             if (firstclause) {
-                sql.append(" and job.status = :status");
+                sql.append(" and job.status = :status" + counter++);
             } else {
                 firstclause = true;
-                sql.append(" job.status = :status");
+                sql.append(" job.status = :status" + counter++);
             }
 
         }
@@ -83,19 +84,19 @@ public class FaxJobDaoImpl extends AbstractDaoImpl<FaxJob> implements FaxJobDao 
 
         if (beginDate != null) {
             if (firstclause) {
-                sql.append(" and job.stamp >= :beginDate");
+                sql.append(" and job.stamp >= :beginDate" + counter++);
             } else {
                 firstclause = true;
-                sql.append(" job.stamp >= :beginDate");
+                sql.append(" job.stamp >= :beginDate" + counter++);
             }
         }
 
         if (endDate != null) {
             if (firstclause) {
-                sql.append(" and job.stamp <= :endDate");
+                sql.append(" and job.stamp <= :endDate" + counter++);
             } else {
                 firstclause = true;
-                sql.append(" job.stamp <= :endDate");
+                sql.append(" job.stamp <= :endDate" + counter++);
             }
         }
 
@@ -109,20 +110,22 @@ public class FaxJobDaoImpl extends AbstractDaoImpl<FaxJob> implements FaxJobDao 
 
         Query query = entityManager.createQuery(sql.toString());
 
+        counter = 1;
+
         if (beginDate != null) {
-            query.setParameter("beginDate", beginDate);
+            query.setParameter(counter++, beginDate);
         }
 
         if (endDate != null) {
-            query.setParameter("endDate", endDate);
+            query.setParameter(counter++, endDate);
         }
 
         if (status != null) {
-            query.setParameter("status", FaxJob.STATUS.valueOf(status));
+            query.setParameter(counter++, FaxJob.STATUS.valueOf(status));
         }
 
         if (demographic_no != null) {
-            query.setParameter("demo", Integer.parseInt(demographic_no));
+            query.setParameter(counter++, Integer.parseInt(demographic_no));
         }
 
         List<FaxJob> faxJobList = query.getResultList();
@@ -136,12 +139,12 @@ public class FaxJobDaoImpl extends AbstractDaoImpl<FaxJob> implements FaxJobDao 
     @Override
     public List<FaxJob> getReadyToSendFaxes(String number) {
         Query query = entityManager.createQuery(
-                "select job from FaxJob job where job.status = :status and job.fax_line = :number and job.jobId is null");
+                "select job from FaxJob job where job.status = ?1 and job.fax_line = ?2 and job.jobId is null");
 
         // these faxes are "waiting" to be sent
         // they become "sent" after they clear the api
-        query.setParameter("status", FaxJob.STATUS.WAITING);
-        query.setParameter("number", number);
+        query.setParameter(1, FaxJob.STATUS.WAITING);
+        query.setParameter(2, number);
 
         return query.getResultList();
     }
@@ -150,10 +153,10 @@ public class FaxJobDaoImpl extends AbstractDaoImpl<FaxJob> implements FaxJobDao 
     @Override
     public List<FaxJob> getInprogressFaxesByJobId() {
         Query query = entityManager.createQuery(
-                "select job from FaxJob job where (job.status = :status or job.status = :status2) and job.jobId is not null");
+                "select job from FaxJob job where (job.status = ?1 or job.status = ?2) and job.jobId is not null");
 
-        query.setParameter("status", FaxJob.STATUS.SENT);
-        query.setParameter("status2", FaxJob.STATUS.WAITING);
+        query.setParameter(1, FaxJob.STATUS.SENT);
+        query.setParameter(2, FaxJob.STATUS.WAITING);
 
         List<FaxJob> faxJobList = query.getResultList();
 
