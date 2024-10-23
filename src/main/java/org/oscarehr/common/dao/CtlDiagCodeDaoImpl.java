c88e0fa4ead1227ff3711296eca9d06de89975d9
garciaerin <garciaerinjade@gmail.com>
2024-10-23 13:59:44 -0400

    fix: corrected positional parameters in CtlDiagCodeDaoImpl.java
    Updated CtlDiagCodeDaoImpl.java
    - added missing numbers beside ? in line 48

--- src/main/java/org/oscarehr/common/dao/CtlDiagCodeDaoImpl.java
+++ src/main/java/org/oscarehr/common/dao/CtlDiagCodeDaoImpl.java
@@ -45,15 +45,15 @@ public class CtlDiagCodeDaoImpl extends AbstractDaoImpl<CtlDiagCode> implements 
     @NativeSql({"ctl_diagcode", "diagnosticcode"})
     public List<Object[]> getDiagnostics(String billRegion, String serviceType) {
         Query query = entityManager.createNativeQuery("SELECT d.diagnostic_code, d.description FROM diagnosticcode d, "
-                + "ctl_diagcode c WHERE d.diagnostic_code=c.diagnostic_code and d.region = ? and c.servicetype = ?");
+                + "ctl_diagcode c WHERE d.diagnostic_code=c.diagnostic_code and d.region = ?1 and c.servicetype = ?2");
         query.setParameter(1, billRegion);
         query.setParameter(2, serviceType);
         return query.getResultList();
     }
 
     public List<CtlDiagCode> findByServiceType(String serviceType) {
-        Query q = entityManager.createQuery("select x from CtlDiagCode x where x.serviceType = ?");
-        q.setParameter(0, serviceType);
+        Query q = entityManager.createQuery("select x from CtlDiagCode x where x.serviceType = ?1");
+        q.setParameter(1, serviceType);
 
         @SuppressWarnings("unchecked")
         List<CtlDiagCode> results = q.getResultList();
